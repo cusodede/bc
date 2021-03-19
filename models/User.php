a8248e1949f3e6bf5b1d613387d1f055fb04d0fc
Pozitronik <antikillerxxl@gmail.com>
2021-03-19 20:32:10 +0300

    namespace fixed

--- models/User.php
+++ models/User.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types = 1);
 
-namespace app\models\user;
+namespace app\models\users;
 
 use app\models\sys\users\Users;
 use yii\base\BaseObject;
@@ -36,7 +36,7 @@ class User extends BaseObject implements IdentityInterface {
 	 * Расширяет свойства Yii::$app->user->identity, абсолютно идентично Users::CurrentUser
 	 * используем вместо Users::CurrentUser, т.к. работает быстрее в счёт кеширования
 	 */
-	public static function fillUserData($data = []) {
+	public static function fillUserData($data = []):void {
 		self::$users = $data;
 		if (!empty($data)) {
 			self::$users = [
@@ -92,7 +92,7 @@ class User extends BaseObject implements IdentityInterface {
 	 * Validates password
 	 *
 	 * @param string $password password to validate
-	 * @return boolean if password provided is valid for current user
+	 * @return bool if password provided is valid for current user
 	 */
 	public function validatePassword($password):bool {
 		return (null === $this->salt)?$this->password === $password:sha1($password.$this->salt) === $this->password;
