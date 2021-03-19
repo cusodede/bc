2881008e35e4c24281719ae70a79764750301073
Pozitronik <antikillerxxl@gmail.com>
2021-03-19 19:16:04 +0300

    Basic error handler action & view

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,6 +3,7 @@ declare(strict_types = 1);
 
 namespace app\controllers;
 
+use Yii;
 use yii\web\Controller;
 
 /**
@@ -12,13 +13,22 @@ use yii\web\Controller;
 class SiteController extends Controller {
 
 	/**
-	 * Displays homepage.
-	 *
 	 * @return string
 	 */
 	public function actionIndex():string {
 		return $this->render('index');
 	}
 
+	public function actionError():string {
+		$exception = Yii::$app->errorHandler->exception;
+
+		if (null !== $exception) {
+			return $this->render('error', [
+				'exception' => $exception
+			]);
+		}
+		return "Status: {$exception->statusCode}";
+
+	}
 
 }
---
+++ views/site/error.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types = 1);
+/**
+ * Шаблон страницы ошибки
+ * @var View $this
+ * @var Throwable $exception
+ */
+
+use yii\helpers\Html;
+use yii\web\View;
+
+$message = $exception->getMessage();
+$this->title = $message;
+?>
+<hr class="new-section-sm bord-no">-
+<div class="row">
+    <div class="col-lg-6 col-lg-offset-3">
+        <div class="panel panel-trans text-center">
+            <div class="panel-heading">
+                <h1 class="error-code text-primary"><?= Html::encode($exception->statusCode) ?></h1>
+            </div>
+            <div class="panel-body">
+                <p><?= nl2br(Html::encode($message)) ?></p>
+                <i class="fa fa-spinner fa-pulse fa-3x fa-fw text-primary"></i>
+                <div class="pad-top"><a class="btn-link text-semibold" href="/">На главную</a></div>
+            </div>
+        </div>
+    </div>
+</div>
