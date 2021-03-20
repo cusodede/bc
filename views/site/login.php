aabc022e3715cd658c192f33eabbba9920814010
Pozitronik <antikillerxxl@gmail.com>
2021-03-20 12:04:09 +0300

    Вычищаю вью

--- views/site/login.php
+++ views/site/login.php
@@ -17,25 +17,15 @@ $this->title = 'Вход';
 ?>
 <div class="cls-content-sm panel">
 	<div class="panel-body">
-		<div class="mar-ver pad-btm">
-			<div class="row">
-				<div class="col-md-4"></div>
-			</div>
-			<h1 class="h3">Вход</h1>
-			<p>Приготовьтесь войти в неизведанное</p>
-		</div>
 		<?php $form = ActiveForm::begin(); ?>
 		<div class="form-group">
-			<?= $form->field($login, 'login')
-				->textInput(['placeholder' => 'Пожалуйста, введите логин']) ?>
+			<?= $form->field($login, 'login')->textInput(['placeholder' => 'Пожалуйста, введите логин']) ?>
 		</div>
 		<div class="form-group">
-			<?= $form->field($login, 'password')
-				->passwordInput(['placeholder' => 'Пожалуйста, введите пароль']) ?>
+			<?= $form->field($login, 'password')->passwordInput(['placeholder' => 'Пожалуйста, введите пароль']) ?>
 		</div>
 		<div class="checkbox pad-btm text-left">
-			<?= $form->field($login, 'rememberMe')
-				->checkbox() ?>
+			<?= $form->field($login, 'rememberMe')->checkbox() ?>
 		</div>
 		<?= Html::submitButton('Войти', ['class' => 'btn btn-primary btn-lg btn-block', 'name' => 'login-button']) ?>
 		<?php ActiveForm::end(); ?>
