0626ebf7ddd58c8aa2498ac86901f9f8978399cc
Pozitronik <antikillerxxl@gmail.com>
2021-03-20 10:27:52 +0300

    Users search model stub

---
+++ models/sys/users/UsersSearch.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\models\sys\users;
+
+use yii\data\ActiveDataProvider;
+
+/**
+ * UsersSearch represents the model behind the search form about `app\models\users\Users`.
+ * Class UsersSearch
+ * @package app\models\users
+ *
+ */
+class UsersSearch extends Users {
+
+	/**
+	 * @inheritdoc
+	 */
+	public function rules():array {
+		return [
+			[['id'], 'integer'],
+			[['username', 'login', 'email'], 'safe'],
+		];
+	}
+
+	/**
+	 * @param array $params
+	 * @param int[] $allowedGroups
+	 * @param bool $pagination
+	 * @return ActiveDataProvider
+	 */
+	public function search(array $params, array $allowedGroups = [], $pagination = true):ActiveDataProvider {
+		$query = Users::find()->active();
+
+		$dataProvider = new ActiveDataProvider([
+			'query' => $query
+		]);
+
+		$dataProvider->setSort([
+			'defaultOrder' => ['id' => SORT_ASC],
+			'attributes' => [
+				'id',
+				'username',
+				'login',
+				'email',
+			]
+		]);
+
+		$this->load($params);
+		if (false === $pagination) $dataProvider->pagination = $pagination;
+
+		if (!$this->validate()) return $dataProvider;
+
+		$query->distinct();
+
+		$query->andFilterWhere(['sys_users.id' => $this->id])
+			->andFilterWhere(['group_id' => $allowedGroups])
+			->andFilterWhere(['like', 'sys_users.username', $this->username])
+			->andFilterWhere(['like', 'login', $this->login])
+			->andFilterWhere(['like', 'email', $this->email]);
+
+		return $dataProvider;
+	}
+}
