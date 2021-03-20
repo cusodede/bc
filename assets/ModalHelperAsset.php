992f19c820fe4b0ad795a45c3fb165b8bd8bf9fb
Pozitronik <antikillerxxl@gmail.com>
2021-03-20 11:03:45 +0300

    ModalHelperAsset

---
+++ assets/ModalHelperAsset.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\targets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Class ModalHelperAsset
+ */
+class ModalHelperAsset extends AssetBundle {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function init():void {
+		$this->sourcePath = __DIR__.'/assets/modalHelper/';
+		$this->css = [
+			'css/modalHelper.css'
+		];
+		$this->js = [
+			'js/modalHelper.js'
+		];
+		parent::init();
+	}
+}
