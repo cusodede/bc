2026d66e41f255cb7e582f4b6a2433ce579ac462
Pozitronik <antikillerxxl@gmail.com>
2021-03-21 18:46:48 +0300

    Инициализация приложения уехала в консольный контроллер

--- controllers/ServiceController.php
+++ controllers/ServiceController.php
@@ -3,7 +3,6 @@ declare(strict_types = 1);
 
 namespace app\controllers;
 
-use app\models\core\Service;
 use yii\base\Response;
 use yii\web\Controller;
 
@@ -19,14 +18,4 @@ class ServiceController extends Controller {
 		return $this->render('index');
 	}
 
-	/**
-	 * @return string
-	 */
-	public function actionReset():string {
-//		Yii::$app->user->logout();
-		return $this->render('reset', [
-			'result' => Service::ResetDB()
-		]);
-
-	}
 }
