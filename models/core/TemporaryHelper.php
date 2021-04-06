8996668f701e7bd936be6115af39e60eb7e9ea8b
Pozitronik <antikillerxxl@gmail.com>
2021-04-06 15:49:21 +0300

    Временный хелпер для прототипирования

---
+++ models/core/TemporaryHelper.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\models\core;
+
+
+/**
+ * Class TemporaryHelper
+ * Если понадобилось быстро сделать хелперную функцию, которую пока непонятно куда - пихаем сюда, потом рефакторим
+ */
+class TemporaryHelper {
+
+
+}
