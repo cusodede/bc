964b4a41942acb9a590b2a610ac0ac526e1d00b6
Pozitronik <antikillerxxl@gmail.com>
2021-04-07 17:58:19 +0300

    Базовая поддержка inline-редактора

--- controllers/PermissionsController.php
+++ controllers/PermissionsController.php
@@ -4,7 +4,9 @@ declare(strict_types = 1);
 namespace app\controllers;
 
 use app\models\sys\permissions\PermissionsSearch;
+use kartik\grid\EditableColumnAction;
 use pozitronik\core\traits\ControllerTrait;
+use pozitronik\helpers\ArrayHelper;
 use Yii;
 use yii\web\Controller;
 
@@ -13,6 +15,37 @@ use yii\web\Controller;
  */
 class PermissionsController extends Controller {
 	use ControllerTrait;
+
+	/**
+	 * @inheritDoc
+	 */
+	public function actions() {
+		$defaultEditableActionConfig = [
+			'class' => EditableColumnAction::class,
+			'modelClass' => PermissionsSearch::class,
+			'showModelErrors' => true,
+		];
+
+		return ArrayHelper::merge(parent::actions(), [
+			/**
+			 * Можно назначить один экшен на все поля, которым не требуется специализированный обработчик,
+			 * данные всё равно грузятся так, будто постится полная форма.
+			 * @see \kartik\grid\EditableColumnAction::validateEditable()
+			 */
+			'editDefault' => $defaultEditableActionConfig,
+			'editAction' => [
+				'class' => EditableColumnAction::class,
+				'modelClass' => PermissionsSearch::class,
+				'showModelErrors' => true,
+//				'outputValue' => function(PermissionsSearch $model, string $attribute, int $key, int $index) {
+//					if ('' === $model->$attribute && null !== $model->controller) {
+//						return 'Все';
+//					}
+//					return '';
+//				},
+			]
+		]);
+	}
 
 	public function actionIndex():string {
 		$params = Yii::$app->request->queryParams;
