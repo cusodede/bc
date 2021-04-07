07085d35ff53edae766252594e9c3f2177d1b12b
Pozitronik <antikillerxxl@gmail.com>
2021-04-07 17:57:46 +0300

    Перечисляю все rest-доступы

--- models/core/TemporaryHelper.php
+++ models/core/TemporaryHelper.php
@@ -3,12 +3,18 @@ declare(strict_types = 1);
 
 namespace app\models\core;
 
-
 /**
  * Class TemporaryHelper
  * Если понадобилось быстро сделать хелперную функцию, которую пока непонятно куда - пихаем сюда, потом рефакторим
  */
 class TemporaryHelper {
 
-
+	const VERBS = [
+		'GET' => 'GET',
+		'HEAD' => 'HEAD',
+		'POST' => 'POST',
+		'PUT' => 'PUT',
+		'PATCH' => 'PATCH',
+		'DELETE' => 'DELETE'
+	];
 }
