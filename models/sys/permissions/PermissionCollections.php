f8bcdb5181a30b99d0e503f2fd8c519ec794f746
Pozitronik <antikillerxxl@gmail.com>
2021-04-08 15:07:36 +0300

    Промежуточный класс, отделённый от AR-класса

---
+++ models/sys/permissions/PermissionCollections.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\models\sys\permissions;
+
+use app\models\sys\permissions\active_record\PermissionsCollections as ActiveRecordPermissionsCollections;
+
+/**
+ * Class PermissionsCollections
+ */
+class PermissionsCollections extends ActiveRecordPermissionsCollections {
+
+}
