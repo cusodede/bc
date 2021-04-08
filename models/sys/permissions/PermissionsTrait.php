5f00d37876097ddde4b730beca24217c7eeedde7
Pozitronik <antikillerxxl@gmail.com>
2021-04-08 15:06:48 +0300

    Свойства для связи с таблицами доступов и коллекций доступов

--- models/sys/permissions/PermissionsTrait.php
+++ models/sys/permissions/PermissionsTrait.php
@@ -4,15 +4,23 @@ declare(strict_types = 1);
 namespace app\models\sys\permissions;
 
 use app\models\core\CacheHelper;
+use app\models\sys\permissions\active_record\relations\RelUsersToPermissions;
+use app\models\sys\permissions\active_record\relations\RelUsersToPermissionsCollections;
 use pozitronik\helpers\ArrayHelper;
 use Throwable;
 use Yii;
 use yii\caching\TagDependency;
+use yii\db\ActiveQuery;
 
 /**
  * Class Permissions
  * Управление правами доступа
  * @property int $id Model primary key attribute name
+ *
+ * @property RelUsersToPermissions[] $relatedUsersToPermissions Связь к промежуточной таблице пользовательских доступов
+ * @property RelUsersToPermissionsCollections[] $relatedUsersToPermissionsCollections Связь к промежуточной таблице наборов пользовательских доступов
+ * @property Permissions[] $relatedPermissions Назначенные напрямую доступы
+ * @property PermissionsCollections[] $relatedPermissionsCollections Назначенные напрямую доступы
  */
 trait PermissionsTrait {
 	/**
@@ -65,6 +73,48 @@ trait PermissionsTrait {
 		return Yii::$app->cache->getOrSet($cacheKey, function() {
 			return Permissions::allUserPermissions($this->id);
 		});
+	}
+
+	/**
+	 * @return ActiveQuery
+	 */
+	public function getRelatedUsersToPermissions():ActiveQuery {
+		return $this->hasMany(RelUsersToPermissions::class, ['user_id' => 'id']);
+	}
+
+	/**
+	 * @return ActiveQuery
+	 */
+	public function getRelatedPermissions():ActiveQuery {
+		return $this->hasMany(RelUsersToPermissions::class, ['permission_id' => 'permission_id'])->via('relatedUsersToPermissions');
+	}
+
+	/**
+	 * @param Permissions[] $relatedPermissions
+	 */
+	public function setRelatedPermissions(array $relatedPermissions):void {
+		//todo
+	}
+
+	/**
+	 * @return ActiveQuery
+	 */
+	public function getRelatedUsersToPermissionsCollections():ActiveQuery {
+		return $this->hasMany(RelUsersToPermissionsCollections::class, ['user_id' => 'id']);
+	}
+
+	/**
+	 * @return ActiveQuery
+	 */
+	public function getRelatedPermissionsCollections():ActiveQuery {
+		return $this->hasMany(RelUsersToPermissionsCollections::class, ['collection_id' => 'collection_id'])->via('relatedUsersToPermissionsCollections');
+	}
+
+	/**
+	 * @param PermissionsCollections[] $relatedPermissionsCollections
+	 */
+	public function setRelatedPermissionsCollections(array $relatedPermissionsCollections):void {
+		//todo
 	}
 
 }
