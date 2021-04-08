f1f51c3931df23950edfe56df56e97773b099cc0
Pozitronik <antikillerxxl@gmail.com>
2021-04-08 11:04:14 +0300

    MultiSelectListBox для выбора прав в наборе

--- views/permissions-collections/subviews/editPanelBody.php
+++ views/permissions-collections/subviews/editPanelBody.php
@@ -7,9 +7,13 @@ declare(strict_types = 1);
  * @var ActiveForm $form
  */
 
+use app\models\sys\permissions\active_record\Permissions;
 use app\models\sys\permissions\active_record\PermissionsCollections;
+use dosamigos\multiselect\MultiSelectListBox;
 use kartik\form\ActiveForm;
+use pozitronik\helpers\ArrayHelper;
 use yii\web\View;
+$this->registerCss(".ms-container {width:100%}");
 
 ?>
 
@@ -21,6 +25,16 @@ use yii\web\View;
 <div class="row">
 	<div class="col-md-12">
 		<?= $form->field($model, 'comment')->textarea() ?>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-12">
+		<?= $form->field($model, 'relatedPermissions')->widget(MultiSelectListBox::class, [
+			'options' => [
+				'multiple' => true,
+			],
+			'data' => ArrayHelper::getColumn(Permissions::find()->all(), 'name'),
+		]) ?>
 	</div>
 </div>
 
