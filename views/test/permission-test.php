4f8a3e162f5c12f751ff7506dcc7e12be9f0894f
Pozitronik <antikillerxxl@gmail.com>
2021-04-09 15:14:55 +0300

    unused

--- views/test/permission-test.php
+++ views/test/permission-test.php
@@ -3,12 +3,9 @@ declare(strict_types = 1);
 
 /**
  * @var View $this
- * @var string[] $actions
  */
 
-use app\controllers\TestController;
 use app\models\sys\users\CurrentUserHelper;
-use yii\helpers\Html;
 use yii\web\View;
 
 ?>
