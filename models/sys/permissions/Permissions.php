e065125346b19c19e5858b0fa52a268a7c8319a7
Pozitronik <antikillerxxl@gmail.com>
2021-04-09 14:05:30 +0300

    allUserPermissions вторым параметром принимает дополнительный набор фильтров

--- models/sys/permissions/Permissions.php
+++ models/sys/permissions/Permissions.php
@@ -3,7 +3,6 @@ declare(strict_types = 1);
 
 namespace app\models\sys\permissions;
 
-
 use app\models\sys\permissions\active_record\Permissions as ActiveRecordPermissions;
 
 /**
@@ -23,17 +22,21 @@ class Permissions extends ActiveRecordPermissions {
 
 	/**
 	 * @param int $user_id
+	 * @param string[] $permissionFilters
 	 * @return self[]
 	 */
-	public static function allUserPermissions(int $user_id):array {
-		return self::find()
+	public static function allUserPermissions(int $user_id, array $permissionFilters = []):array {
+		$query = self::find()
 			->distinct()
 			->joinWith(['relatedUsersToPermissions directPermissions', 'relatedUsersToPermissionsCollections collectionPermissions'], false)
 			->where(['directPermissions.user_id' => $user_id])
 			->orWhere(['collectionPermissions.user_id' => $user_id])
 			->orderBy([
 				'priority' => SORT_DESC,
-				'id' => SORT_ASC])
-			->all();
+				'id' => SORT_ASC]);
+		foreach ($permissionFilters as $paramName => $paramValue) {
+			$query->andFilterWhere([self::tableName().".".$paramName => $paramValue]);
+		}
+		return $query->all();
 	}
 }
