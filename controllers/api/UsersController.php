a1000fbaa7b53812e4200742c503506d3a6cabcd
Pozitronik <antikillerxxl@gmail.com>
2021-04-10 13:10:01 +0300

    PermissionFilter работает в rest-контроллерах

--- controllers/api/UsersController.php
+++ controllers/api/UsersController.php
@@ -3,12 +3,47 @@ declare(strict_types = 1);
 
 namespace app\controllers\api;
 
+use app\models\sys\permissions\filters\PermissionFilter;
 use app\models\sys\users\Users;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\ContentNegotiator;
+use yii\filters\RateLimiter;
+use yii\filters\VerbFilter;
 use yii\rest\ActiveController;
+use yii\web\Response;
 
 /**
  * Class UserController
  */
 class UsersController extends ActiveController {
 	public $modelClass = Users::class;
+
+	/**
+	 * @inheritDoc
+	 */
+	public function behaviors():array {
+		return [
+			'contentNegotiator' => [
+				'class' => ContentNegotiator::class,
+				'formats' => [
+					'application/json' => Response::FORMAT_JSON,
+					'application/xml' => Response::FORMAT_XML,
+				],
+			],
+			'verbFilter' => [
+				'class' => VerbFilter::class,
+				'actions' => $this->verbs(),
+			],
+			'authenticator' => [
+				'class' => CompositeAuth::class,
+			],
+			'rateLimiter' => [
+				'class' => RateLimiter::class,
+			],
+			'access' => [
+				'class' => PermissionFilter::class
+			]
+		];
+
+	}
 }
