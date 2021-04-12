ec253157e97330ef4ce92bc99710fc5cadb58888
Pozitronik <antikillerxxl@gmail.com>
2021-04-12 22:20:28 +0300

    Модель смены пароля пользователя

---
+++ models/site/UpdatePasswordForm.php
@@ -0,0 +1,89 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\models\site;
+
+use app\models\sys\users\Users;
+use yii\base\InvalidConfigException;
+use yii\base\Model;
+
+/**
+ * Модель смены пароля пользователя.
+ *
+ * @property Users $user
+ * @property string $oldPassword Предыдущий пароль
+ * @property string $newPassword Новый пароль
+ * @property string $newPasswordRepeat Повтор пароля для самопроверки
+ */
+class UpdatePasswordForm extends Model {
+	public $oldPassword;
+	public $newPassword;
+	public $newPasswordRepeat;
+
+	/** @var Users */
+	private $_user;
+
+	/**
+	 * @inheritDoc
+	 */
+	public function rules():array {
+		return [
+			[['oldPassword', 'newPassword', 'newPasswordRepeat'], 'required'],
+			[['newPasswordRepeat'], function(string $attribute):void {
+				if (!$this->hasErrors() && $this->newPassword !== $this->newPasswordRepeat) {
+					$this->addError('newPasswordRepeat', 'Введённые пароли должны совпадать');
+				}
+			}],
+			[['oldPassword'], function(string $attribute):void {
+				if (!$this->hasErrors() && !$this->user->validatePassword($this->oldPassword)) {
+					$this->addError('oldPassword', 'Текущий пароль введён неверно');
+				}
+			}]
+
+		];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function attributeLabels():array {
+		return [
+			'oldPassword' => 'Текущий пароль',
+			'newPassword' => 'Новый пароль',
+			'newPasswordRepeat' => 'Проверочный ввод нового пароля'
+		];
+	}
+
+	/**
+	 * Обновляет пароль пользователя
+	 * @return bool
+	 */
+	public function doUpdate():bool {
+		if ($this->validate()) {
+			$this->user->password = $this->newPassword;
+			$this->user->is_pwd_outdated = false;
+			return $this->user->save();
+		}
+		return false;
+	}
+
+	/**
+	 * @return Users
+	 * @throws InvalidConfigException
+	 */
+	public function getUser():Users {
+		if (null === $this->_user) {
+			throw new InvalidConfigException('Не указан пользователь для смены пароля');
+		}
+		return $this->_user;
+	}
+
+	/**
+	 * @param Users $user
+	 * @return void
+	 */
+	public function setUser(Users $user):void {
+		$this->_user = $user;
+	}
+
+}
