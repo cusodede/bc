dd2f4923e783ab29729c6e3dcb61b450e6feee93
Pozitronik <antikillerxxl@gmail.com>
2021-04-12 22:23:11 +0300

    Экшен смены пароля

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -4,13 +4,17 @@ declare(strict_types = 1);
 namespace app\controllers;
 
 use app\models\site\LoginForm;
+use app\models\site\UpdatePasswordForm;
+use app\models\sys\users\Users;
 use pozitronik\core\traits\ControllerTrait;
 use pozitronik\helpers\ArrayHelper;
+use pozitronik\sys_exceptions\models\LoggedException;
 use Throwable;
 use Yii;
 use yii\web\Controller;
 use yii\web\ErrorAction;
 use yii\web\Response;
+use yii\web\UnauthorizedHttpException;
 
 /**
  * Class SiteController
@@ -38,6 +42,9 @@ class SiteController extends Controller {
 		$this->layout = 'login';
 		$model = new LoginForm();
 		if ($model->load(Yii::$app->request->post()) && $model->doLogin()) {
+			if ($model->user->is_pwd_outdated) {
+				return $this->redirect('update-password');
+			}
 			return $this->redirect(Yii::$app->homeUrl);
 		}
 		return $this->render('login', [
@@ -45,9 +52,38 @@ class SiteController extends Controller {
 		]);
 	}
 
+	/**
+	 * logout
+	 */
 	public function actionLogout():void {
 		Yii::$app->user->logout();
 		$this->redirect('index');
+	}
+
+	/**
+	 * На момент входа в экшен пользователь должен быть авторизован.
+	 * Если при этом у него протухший пароль (перекинуло из actionLogin()), то вызывается форма смены пароля,
+	 *
+	 * Пользователь остаётся авторизован (это нужно для проброса данных смены пароля), но его не пускает
+	 * в основном шаблоне приложения
+	 *
+	 * @return string|Response
+	 * @throws LoggedException
+	 */
+	public function actionUpdatePassword() {
+		$this->layout = 'login';
+		/** @var Users|null $loggedUser */
+		if (null === $loggedUser = Yii::$app->user->identity) {
+			throw new LoggedException(new UnauthorizedHttpException('Пользователь не авторизован'));
+		}
+
+		$updatePasswordModel = new UpdatePasswordForm(['user' => $loggedUser]);
+		if ($updatePasswordModel->load(Yii::$app->request->post()) && $updatePasswordModel->doUpdate()) {
+			return $this->redirect(Yii::$app->homeUrl);
+		}
+		return $this->render('update-password', [
+			'model' => $updatePasswordModel
+		]);
 	}
 
 	/**
