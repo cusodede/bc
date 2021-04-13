c45491cd2461cc01ba2606652020d11b7e4240bb
Pozitronik <antikillerxxl@gmail.com>
2021-04-13 22:25:17 +0300

    Вьюха восстановления пароля

---
+++ views/site/restore-password.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * Шаблон страницы восстановления пароля
+ * @var View $this
+ * @var ActiveForm $form
+ * @var RestorePasswordForm $model
+ */
+
+use app\models\site\RestorePasswordForm;
+use yii\web\View;
+use yii\widgets\ActiveForm;
+use yii\helpers\Html;
+
+$this->title = 'Восстановление пароля';
+?>
+<div class="panel">
+	<div class="panel-body">
+		<?php $form = ActiveForm::begin(); ?>
+		<div class="form-group">
+			<?= $form->field($model, 'email')->passwordInput(['placeholder' => 'Почтовый адрес, указанный при регистрации']) ?>
+		</div>
+		<?= Html::submitButton('Восстановить пароль', ['class' => 'btn btn-primary btn-lg btn-block', 'name' => 'login-button']) ?>
+		<?php ActiveForm::end(); ?>
+		<?= Html::a('Назад', Yii::$app->homeUrl, ['class' => 'btn-link mar-lft']) ?>
+	</div>
+</div>
