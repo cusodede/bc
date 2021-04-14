08a4b5b92f643f63c9836f2a5e4a89b5c983e62c
Pozitronik <antikillerxxl@gmail.com>
2021-04-14 11:46:26 +0300

    Форма регистрации

---
+++ views/site/register.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * Шаблон страницы регистрации
+ * @var View $this
+ * @var ActiveForm $form
+ * @var RegistrationForm $model
+ */
+
+use app\models\site\RegistrationForm;
+use yii\web\View;
+use yii\widgets\ActiveForm;
+use yii\helpers\Html;
+
+$this->title = 'Вход';
+?>
+<div class="panel">
+	<div class="panel-body">
+		<?php $form = ActiveForm::begin(); ?>
+		<div class="form-group">
+			<?= $form->field($model, 'username')->textInput(['placeholder' => 'Себастьян Перейра']) ?>
+		</div>
+		<div class="form-group">
+			<?= $form->field($model, 'login')->textInput(['placeholder' => 'Логин для входа']) ?>
+		</div>
+		<div class="form-group">
+			<?= $form->field($model, 'email')->textInput(['placeholder' => 'esteban@domain', 'type' => 'email']) ?>
+		</div>
+		<div class="form-group">
+			<?= $form->field($model, 'password')->passwordInput(['placeholder' => 'Пожалуйста, введите пароль']) ?>
+		</div>
+		<div class="form-group">
+			<?= $form->field($model, 'passwordRepeat')->passwordInput(['placeholder' => 'Пароль ещё раз']) ?>
+		</div>
+		<?= Html::submitButton('Зарегистрироваться', ['class' => 'btn btn-primary btn-lg btn-block', 'name' => 'login-button']) ?>
+		<?php ActiveForm::end(); ?>
+	</div>
+</div>
