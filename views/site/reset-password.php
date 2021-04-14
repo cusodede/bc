e73ec88a7c4579e6c438c12e0af4185ae7ed9465
Pozitronik <antikillerxxl@gmail.com>
2021-04-14 10:24:29 +0300

    Форма сброса пароля

---
+++ views/site/reset-password.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * Шаблон страницы сброса пароля
+ * @var View $this
+ * @var ActiveForm $form
+ * @var UpdatePasswordForm $model
+ * @var string $code Код сброса пароля
+ */
+
+use app\controllers\SiteController;
+use app\models\site\UpdatePasswordForm;
+use yii\web\View;
+use yii\widgets\ActiveForm;
+use yii\helpers\Html;
+
+$this->title = 'Смена пароля';
+?>
+<div class="panel">
+	<div class="panel-body">
+		<?php $form = ActiveForm::begin([
+			'action' => SiteController::to('reset-password', ['code' => $code]),
+			'method' => 'post'
+		]); ?>
+		<div class="form-group">
+			<?= $form->field($model, 'newPassword')->passwordInput(['placeholder' => 'Новый пароль']) ?>
+		</div>
+		<div class="form-group">
+			<?= $form->field($model, 'newPasswordRepeat')->passwordInput(['placeholder' => 'Новый пароль ещё раз']) ?>
+		</div>
+		<?= Html::submitButton('Сменить пароль', ['class' => 'btn btn-primary btn-lg btn-block', 'name' => 'login-button']) ?>
+		<?php ActiveForm::end(); ?>
+		<?= Html::a('Назад', Yii::$app->homeUrl, ['class' => 'btn-link mar-lft']) ?>
+	</div>
+</div>
