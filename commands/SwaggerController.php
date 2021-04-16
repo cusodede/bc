9b5e9ad725e7bd4b0b791e07217df01fb4c9578a
Pozitronik <antikillerxxl@gmail.com>
2021-04-16 08:36:04 +0300

    Конвертер умеет обходить директории

--- commands/SwaggerController.php
+++ commands/SwaggerController.php
@@ -5,6 +5,10 @@ namespace app\commands;
 
 use app\models\core\prototypes\PHPDocParser;
 use app\models\core\prototypes\SwaggerConverter;
+use Exception;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use Throwable;
 use Yii;
 use yii\console\Controller;
 use yii\helpers\Console;
@@ -16,13 +20,39 @@ use yii\helpers\Console;
 class SwaggerController extends Controller {
 
 	/**
-	 * Инициализирует приложение
+	 * Конвертирует атрибуты из PHPDoc-блока описания класса в определения @SWG\Definition
+	 * @param string $path
 	 * @return void
+	 * @throws Exception
 	 */
 	public function actionModel(string $path):void {
 		$realPath = Yii::getAlias($path);
-		if (false === $fileContents = file($realPath)) {
-			Console::output(Console::renderColoredString("%rФайл {$realPath} не найден.%n"));
+		if (is_dir($realPath)) {
+			$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($realPath), RecursiveIteratorIterator::SELF_FIRST);
+			/** @var RecursiveDirectoryIterator $file */
+			foreach ($files as $file) {
+				if ($file->isFile() && 'php' === $file->getExtension()) {
+					$this->fromModel($file->getRealPath());
+				}
+			}
+		} else {
+			$this->fromModel($realPath);
+		}
+
+	}
+
+	/**
+	 * @param string $path
+	 * @throws Exception
+	 */
+	private function fromModel(string $path):void {
+		try {
+			$fileContents = file($path);
+		} catch (Throwable $t) {
+			$fileContents = false;
+		}
+		if (false === $fileContents) {
+			Console::output(Console::renderColoredString("%rФайл {$path} не найден.%n"));
 		} else {
 			$parsers = [];
 			$properties = PHPDocParser::ExtractAttributeLines($fileContents);
@@ -30,11 +60,15 @@ class SwaggerController extends Controller {
 				$parser = new PHPDocParser();
 				$parser->loadString($propertyLine);
 				$parsers[] = $parser;
-				Console::output(Console::renderColoredString("%b{$parser}%n"));
+				//Console::output(Console::renderColoredString("%b{$parser}%n"));
 			}
 			$converter = new SwaggerConverter(['parsers' => $parsers]);
+			if ([] !== $parsers) {
+				Console::output(Console::renderColoredString("%Y{$path}:%n"));
+				Console::output(Console::renderColoredString("%g{$converter}%n"));
+			}
 
-			Console::output(Console::renderColoredString("%g{$converter}%n"));
 		}
 	}
+
 }
