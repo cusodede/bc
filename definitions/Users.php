e362fe91cd59dad39c2a377db2c91e31b5a7a5ec
Pozitronik <antikillerxxl@gmail.com>
2021-04-16 08:58:32 +0300

    Описания, сгенеренные конвертером

--- definitions/Users.php
+++ definitions/Users.php
@@ -4,11 +4,20 @@ declare(strict_types = 1);
 namespace app\definitions;
 
 /**
- * @SWG\Definition(required={"username", "email"})
+ * @SWG\Definition(required={"restore_code", "salt"})
  *
  * @SWG\Property(property="id", type="integer")
- * @SWG\Property(property="email", type="string")
  * @SWG\Property(property="username", type="string")
+ * @SWG\Property(property="login", type="string")
+ * @SWG\Property(property="password", type="string")
+ * @SWG\Property(property="restore_code", type="string")
+ * @SWG\Property(property="salt", type="string")
+ * @SWG\Property(property="is_pwd_outdated", type="boolean")
+ * @SWG\Property(property="email", type="string")
+ * @SWG\Property(property="comment", type="string")
+ * @SWG\Property(property="create_date", type="string")
+ * @SWG\Property(property="daddy", type="integer")
+ * @SWG\Property(property="deleted", type="boolean")
  */
 class Users {
 }
