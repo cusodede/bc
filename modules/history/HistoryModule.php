c3b09daa7cba46b55b40b9b2cc048125f72441dd
Pozitronik <antikillerxxl@gmail.com>
2021-04-18 09:34:51 +0300

    Инициализируем модуль истории

---
+++ modules/history/HistoryModule.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\history;
+
+use pozitronik\core\traits\ModuleExtended;
+use yii\base\Module;
+
+/**
+ * Class HistoryModule
+ * @package app\modules\history
+ */
+class HistoryModule extends Module {
+	use ModuleExtended;
+}
