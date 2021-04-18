77cb01b0774fc7e4e86c59d3bfa7a5ad85a3c64b
Pozitronik <antikillerxxl@gmail.com>
2021-04-18 10:01:48 +0300

    AR-класс таблицы меток событий

---
+++ modules/history/models/active_record/HistoryTags.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\history\models\active_record;
+
+use app\modules\history\HistoryModule;
+use pozitronik\core\helpers\ModuleHelper;
+use pozitronik\helpers\ArrayHelper;
+use yii\db\ActiveRecord;
+
+/**
+ * Class HistoryTags
+ * Просто метки событий
+ * @property int $id
+ * @property int $history
+ * @property string $tag
+ */
+class HistoryTags extends ActiveRecord {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public static function tableName():string {
+		return ArrayHelper::getValue(ModuleHelper::params(HistoryModule::class), 'tableNameTags', 'sys_history_tags');
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules():array {
+		return [
+			[['history', 'tag'], 'required'],
+			[['tag'], 'string'],
+			[['history'], 'integer'],
+			[['history', 'tag'], 'unique', 'targetAttribute' => ['history', 'tag']]
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels():array {
+		return [
+			'id' => 'ID',
+			'history' => 'Идентификатор операции',
+			'tag' => 'Метка'
+		];
+	}
+}
