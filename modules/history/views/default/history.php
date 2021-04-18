2c886bbad9c7b36aa740560d83b5088ad1c0d10d
Pozitronik <antikillerxxl@gmail.com>
2021-04-18 10:59:25 +0300

    Шаблоны просмотра истории

---
+++ modules/history/views/default/history.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * @var View $this
+ * @var int $for
+ * @var int $id
+ * @var int $level
+ * @var int $levelCount
+ * @var ActiveRecord $model
+ */
+
+use yii\data\Pagination;
+use yii\db\ActiveRecord;
+use yii\web\View;
+use yii\widgets\DetailView;
+use yii\widgets\LinkPager;
+
+?>
+
+<?= LinkPager::widget([
+	'pagination' => new Pagination([
+		'totalCount' => $levelCount-1,
+		'page' => $level-1,
+		'pageSize' => 1,
+		'pageParam' => 'level',
+		'pageSizeParam' => false,
+//		'route' => HistoryModule::to(['/default/history', 'for' => $for, 'id' => $id])//todo: не знаю как, нет времени фиксить
+	]),
+	'hideOnSinglePage' => false
+]) ?>
+
+<?= DetailView::widget([
+	'model' => $model
+]) ?>
