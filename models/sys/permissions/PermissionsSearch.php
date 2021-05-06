27adaa37f548d296ce9c0888581be65cbfad5710
bm <PSuares@beeline.ru>
2021-05-06 17:15:25 +0300

    issue_3 - search in permissions

--- models/sys/permissions/PermissionsSearch.php
+++ models/sys/permissions/PermissionsSearch.php
@@ -11,6 +11,17 @@ use yii\data\ActiveDataProvider;
 final class PermissionsSearch extends Permissions {
 
 	/**
+	 * @inheritDoc
+	 */
+	public function rules():array {
+		return [
+			['id', 'integer'],
+			['priority', 'integer', 'min' => 0, 'max' => 100],
+			[['name', 'controller', 'action', 'verb'], 'string', 'max' => 255]
+		];
+	}
+
+	/**
 	 * @param array $params
 	 * @return ActiveDataProvider
 	 */
@@ -21,18 +32,37 @@ final class PermissionsSearch extends Permissions {
 			'query' => $query
 		]);
 
-		$dataProvider->setSort([
-			'defaultOrder' => ['id' => SORT_ASC],
-			'attributes' => [
-				'id'
-			]
-		]);
-
+		$this->setSort($dataProvider);
 		$this->load($params);
 
 		if (!$this->validate()) return $dataProvider;
 
+		$this->filterData($query);
+
 		return $dataProvider;
+	}
+
+	/**
+	 * @param $query
+	 * @return void
+	 */
+	private function filterData($query):void {
+		$query->andFilterWhere([self::tableName().'.id' => $this->id])
+			->andFilterWhere([self::tableName().'.priority' => $this->priority])
+			->andFilterWhere(['like', self::tableName().'.name', $this->name])
+			->andFilterWhere(['like', self::tableName().'.controller', $this->controller])
+			->andFilterWhere(['like', self::tableName().'.action', $this->action])
+			->andFilterWhere([self::tableName().'.verb' => $this->verb]);
+	}
+
+	/**
+	 * @param $dataProvider
+	 */
+	private function setSort($dataProvider):void {
+		$dataProvider->setSort([
+			'defaultOrder' => ['id' => SORT_ASC],
+			'attributes' => ['id', 'name', 'controller', 'action', 'verb', 'priority']
+		]);
 	}
 
 }
