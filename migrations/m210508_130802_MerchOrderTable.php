7113b90f063b812cbb2c0052aa6e831c7568daba
Pozitronik <antikillerxxl@gmail.com>
2021-05-08 18:06:41 +0300

    полу статуса

--- migrations/m210508_130802_MerchOrderTable.php
+++ migrations/m210508_130802_MerchOrderTable.php
@@ -14,12 +14,14 @@ class m210508_130802_MerchOrderTable extends Migration {
 			'id' => $this->primaryKey(),
 			'initiator' => $this->integer()->notNull()->comment('Заказчик'),
 			'store' => $this->integer()->notNull()->comment('Магазин'),
+			'status' => $this->integer()->notNull()->comment('Статус'),
 			'create_date' => $this->dateTime()->notNull()->comment('Дата регистрации'),
 			'deleted' => $this->boolean()->notNull()->defaultValue(0)
 		]);
 
 		$this->createIndex('initiator', 'merch_order', 'initiator');
 		$this->createIndex('store', 'merch_order', 'store');
+		$this->createIndex('status', 'merch_order', 'status');
 		$this->createIndex('deleted', 'merch_order', 'deleted');
 
 	}
