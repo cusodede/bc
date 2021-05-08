4058d219a389c61ba498df71ab1dac63f1f37cb5
Pozitronik <antikillerxxl@gmail.com>
2021-05-08 16:22:47 +0300

    Базовая модель справочника товаров

---
+++ models/prototypes/merch/active_record/Merch.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\models\prototypes\merch\active_record;
+
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "merch".
+ *
+ * @property int $id
+ * @property int $name Название товара
+ * @property string $item_class Класс товара
+ * @property string $create_date Дата регистрации
+ * @property int $deleted
+ */
+class Merch extends ActiveRecord {
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName() {
+		return 'merch';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules() {
+		return [
+			[['name', 'item_class', 'create_date'], 'required'],
+			[['name', 'deleted'], 'integer'],
+			[['create_date'], 'safe'],
+			[['item_class'], 'string', 'max' => 255],
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels() {
+		return [
+			'id' => 'ID',
+			'name' => 'Название товара',
+			'item_class' => 'Класс товара',
+			'create_date' => 'Дата регистрации',
+			'deleted' => 'Deleted',
+		];
+	}
+}
