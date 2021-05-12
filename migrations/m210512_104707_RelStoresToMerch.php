84d279cbb7c624cbefb14d20049b2abc2e9b8202
Pozitronik <antikillerxxl@gmail.com>
2021-05-12 13:49:03 +0300

    relation_stores_to_merch migration

---
+++ migrations/m210512_104707_RelStoresToMerch.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types = 1);
+use app\migrations\template\RelationMigration;
+
+/**
+ * Class m210512_104707_RelStoresToMerch
+ */
+class m210512_104707_RelStoresToMerch extends RelationMigration {
+
+	public string $table_name = "relation_stores_to_merch";
+	public string $first_key = "store_id";
+	public string $second_key = "merch_id";
+
+}
