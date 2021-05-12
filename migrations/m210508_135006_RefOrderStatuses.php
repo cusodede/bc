2768523689969a4a0bae98aa3e713492ac94a0c3
Pozitronik <antikillerxxl@gmail.com>
2021-05-12 13:24:22 +0300

    Миграция на справочник типов магазинов

--- migrations/m210508_135006_RefOrderStatuses.php
+++ migrations/m210508_135006_RefOrderStatuses.php
@@ -1,32 +1,10 @@
 <?php
 declare(strict_types = 1);
-use yii\db\Migration;
 
 /**
  * Class m210508_135006_RefOrderStatuses
  */
-class m210508_135006_RefOrderStatuses extends Migration {
-	/**
-	 * {@inheritdoc}
-	 */
-	public function safeUp() {
-		$this->createTable('ref_merch_order_statuses', [
-			'id' => $this->primaryKey(),
-			'name' => $this->string(255)->notNull(),
-			'color' => $this->string(255)->null(),
-			'textcolor' => $this->string(255)->null(),
-			'deleted' => $this->boolean()->notNull()->defaultValue(false)
-		]);
+class m210508_135006_RefOrderStatuses extends CustomisableReferenceMigration {
 
-		$this->createIndex('deleted', 'ref_merch_order_statuses', 'deleted');
-
-	}
-
-	/**
-	 * {@inheritdoc}
-	 */
-	public function safeDown() {
-		$this->dropTable('ref_merch_order_statuses');
-	}
-
+	public $table_name = "reference_store_types";
 }
