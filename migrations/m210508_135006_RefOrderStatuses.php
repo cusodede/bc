e29b7da4a4e95b1bea0932fb0cfc515ed9090b74
Pozitronik <antikillerxxl@gmail.com>
2021-05-13 12:30:56 +0300

    Ссылаемся на модульную миграцию

--- migrations/m210508_135006_RefOrderStatuses.php
+++ migrations/m210508_135006_RefOrderStatuses.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types = 1);
-use app\migrations\template\CustomisableReferenceMigration;
+use pozitronik\references\models\CustomisableReferenceMigration;
 
 /**
  * Class m210508_135006_RefOrderStatuses
