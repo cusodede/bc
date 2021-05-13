3e6f8da208f0a74ca2b4fcfdaba0a52cfe9fbd37
Pozitronik <antikillerxxl@gmail.com>
2021-05-13 13:48:33 +0300

    Модель связи магазинов с продавцами

---
+++ models/prototypes/seller/active_record/relations/RelStoresToSellers.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\models\prototypes\merch\active_record\relations;
+
+use pozitronik\core\traits\Relations;
+use yii\db\ActiveRecord;
+
+/**
+ *
+ * Связь магазинов с продавцами
+ * @property int $id
+ * @property int $store_id
+ * @property int $seller_id
+ */
+class RelStoresToSellers extends ActiveRecord {
+	use Relations;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName():string {
+		return 'relation_store_to_seller';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules():array {
+		return [
+			[['store_id', 'seller_id'], 'required'],
+			[['store_id', 'seller_id'], 'integer'],
+			[['store_id', 'seller_id'], 'unique', 'targetAttribute' => ['store_id', 'seller_id']],
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels():array {
+		return [
+			'id' => 'ID',
+			'store_id' => 'Store ID',
+			'seller_id' => 'Seller ID',
+		];
+	}
+}
