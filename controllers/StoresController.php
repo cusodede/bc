d59e9980f407ac83f54db40de801cb15ae8dd643
Pozitronik <antikillerxxl@gmail.com>
2021-05-13 19:53:31 +0300

    Стабы вью управления магазинами

--- controllers/StoresController.php
+++ controllers/StoresController.php
@@ -7,6 +7,8 @@ use app\models\core\prototypes\DefaultController;
 use app\models\prototypes\seller\Stores;
 use app\models\prototypes\seller\StoresSearch;
 use pozitronik\core\traits\ControllerTrait;
+use Yii;
+use yii\base\InvalidConfigException;
 
 /**
  * Class SimCardController
@@ -16,5 +18,31 @@ class StoresController extends DefaultController {
 
 	public string $modelClass = Stores::class;
 	public string $modelSearchClass = StoresSearch::class;
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getViewPath():string {
+		return '@app/views/stores';
+	}
+
+	/**
+	 * @return string
+	 * @throws InvalidConfigException
+	 * @noinspection PhpPossiblePolymorphicInvocationInspection
+	 */
+	public function actionIndex():string {
+		$params = Yii::$app->request->queryParams;
+		$searchModel = new StoresSearch();
+
+		/** @noinspection PhpUndefinedMethodInspection */
+		return $this->render('index', [
+				'searchModel' => $searchModel,
+				'dataProvider' => $searchModel->search($params),
+				'controller' => $this,
+				'modelName' => (new Stores())->formName()
+			]
+		);
+	}
 
 }
