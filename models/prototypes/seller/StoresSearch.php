7e535e1fd64966811ebafa92b7fa8370c991289f
Pozitronik <antikillerxxl@gmail.com>
2021-05-13 14:17:44 +0300

    Стаб поисковой модели магазинов

---
+++ models/prototypes/seller/StoresSearch.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\models\prototypes\seller;
+
+use app\models\prototypes\seller\active_record\Stores as ActiveRecordStores;
+use yii\data\ActiveDataProvider;
+
+/**
+ * Class StoresSearch
+ * todo
+ */
+class StoresSearch extends ActiveRecordStores {
+	/**
+	 * @param array $params
+	 * @return ActiveDataProvider
+	 */
+	public function search(array $params):ActiveDataProvider {
+		$query = self::find()->active();
+
+		$dataProvider = new ActiveDataProvider([
+			'query' => $query
+		]);
+
+		$dataProvider->setSort([
+			'defaultOrder' => ['id' => SORT_ASC],
+			'attributes' => [
+			]
+		]);
+
+		$this->load($params);
+
+		if (!$this->validate()) return $dataProvider;
+
+		return $dataProvider;
+	}
+}
