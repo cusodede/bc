792657d68e714d3ce31ec33eb8e83b1dee25fe53
bm <PSuares@beeline.ru>
2021-05-14 10:05:22 +0300

    small fix in selector
    (cherry picked from commit 9fd3996b622934a627a288a7b10890d25dd98275)

--- assets/assets/permissionsCollections/js/permissionsCollections.js
+++ assets/assets/permissionsCollections/js/permissionsCollections.js
@@ -5,7 +5,7 @@
 document.addEventListener('modalIsReady', function() {
 	document.getElementById('search-permission').addEventListener('keyup', function() {
 		let reg = new RegExp(this.value, 'i');
-		$('#ms-permissionscollections-relatedpermissions li:not(.ms-selected)').find('span').each(function() {
+		$('#ms-permissionscollections-relatedpermissions .ms-list:first').find('li:not(.ms-selected) span').each(function() {
 			if (this.innerHTML.search(reg) !== -1) {
 				$(this).parent().show();
 			} else {
