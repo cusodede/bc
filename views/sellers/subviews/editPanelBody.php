d6f895ceb6a93fb524eccbb43693c192eddcd50a
Pozitronik <antikillerxxl@gmail.com>
2021-05-16 16:50:51 +0300

    Редактор связей магазинов через SelectModelWidget

--- views/sellers/subviews/editPanelBody.php
+++ views/sellers/subviews/editPanelBody.php
@@ -7,19 +7,30 @@ declare(strict_types = 1);
  * @var ActiveForm $form
  */
 
-use app\models\core\prototypes\ActiveFieldMap;
+use app\controllers\SellersController;
 use app\models\prototypes\seller\Sellers;
+use app\models\prototypes\seller\Stores;
+use app\widgets\selectmodelwidget\SelectModelWidget;
 use kartik\form\ActiveForm;
 use yii\web\View;
 
 ?>
 
-<?php foreach ($model->attributes() as $attribute): ?>
-	<?php if ($model->isAttributeRequired($attribute)): ?>
-		<div class="row">
-			<div class="col-md-12">
-				<?= $form->field($model, $attribute)->widget(ActiveFieldMap::class) ?>
-			</div>
-		</div>
-	<?php endif ?>
-<?php endforeach; ?>
+<div class="row">
+	<div class="col-md-12">
+		<?= $form->field($model, 'name')->textInput() ?>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-12">
+		<?= $form->field($model, 'stores')->widget(SelectModelWidget::class, [
+			'selectModelClass' => Stores::class,
+			'selectionQuery' => Stores::find()->active(),
+//			'pluginOptions' => [
+//				'matcher' => new JsExpression('function(params, data) {return SellersMatchCustom(params, data)}')
+//			],
+			'options' => ['placeholder' => ''],
+			'ajaxSearchUrl' => SellersController::to('ajax-search')
+		]) ?>
+	</div>
+</div>
