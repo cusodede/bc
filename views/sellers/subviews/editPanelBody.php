74b373aaa1658c619d3260fa4a73230b0749c6cd
Pozitronik <antikillerxxl@gmail.com>
2021-05-16 17:08:02 +0300

    SelectModelWidget в ajax

--- views/sellers/subviews/editPanelBody.php
+++ views/sellers/subviews/editPanelBody.php
@@ -7,7 +7,7 @@ declare(strict_types = 1);
  * @var ActiveForm $form
  */
 
-use app\controllers\SellersController;
+use app\controllers\StoresController;
 use app\models\prototypes\seller\Sellers;
 use app\models\prototypes\seller\Stores;
 use app\widgets\selectmodelwidget\SelectModelWidget;
@@ -24,13 +24,14 @@ use yii\web\View;
 <div class="row">
 	<div class="col-md-12">
 		<?= $form->field($model, 'stores')->widget(SelectModelWidget::class, [
+			'loadingMode' => SelectModelWidget::DATA_MODE_AJAX,
 			'selectModelClass' => Stores::class,
-			'selectionQuery' => Stores::find()->active(),
+//			'selectionQuery' => Stores::find()->active(),
 //			'pluginOptions' => [
 //				'matcher' => new JsExpression('function(params, data) {return SellersMatchCustom(params, data)}')
 //			],
 			'options' => ['placeholder' => ''],
-			'ajaxSearchUrl' => SellersController::to('ajax-search')
+			'ajaxSearchUrl' => StoresController::to('ajax-search')
 		]) ?>
 	</div>
 </div>
