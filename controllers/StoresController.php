00797124aefca73b04e1ce2d23c154da731467b3
Pozitronik <antikillerxxl@gmail.com>
2021-05-16 15:25:08 +0300

    Методы можно оставить дефолтными, нужно переделывать только вьюхи

--- controllers/StoresController.php
+++ controllers/StoresController.php
@@ -7,10 +7,6 @@ use app\models\core\prototypes\DefaultController;
 use app\models\prototypes\seller\Stores;
 use app\models\prototypes\seller\StoresSearch;
 use pozitronik\core\traits\ControllerTrait;
-use Throwable;
-use Yii;
-use yii\base\InvalidConfigException;
-use yii\web\Response;
 
 /**
  * Class SimCardController
@@ -28,42 +24,5 @@ class StoresController extends DefaultController {
 		return '@app/views/stores';
 	}
 
-	/**
-	 * @return string
-	 * @throws InvalidConfigException
-	 * @noinspection PhpPossiblePolymorphicInvocationInspection
-	 */
-	public function actionIndex():string {
-		$params = Yii::$app->request->queryParams;
-		$searchModel = new StoresSearch();
-
-		/** @noinspection PhpUndefinedMethodInspection */
-		return $this->render('index', [
-				'searchModel' => $searchModel,
-				'dataProvider' => $searchModel->search($params),
-				'controller' => $this,
-				'modelName' => (new Stores())->formName()
-			]
-		);
-	}
-
-	/**
-	 * @return string|Response
-	 * @throws Throwable
-	 */
-	public function actionCreate() {
-		$model = $this->getModel();
-		if ($model->createModelFromPost()) {
-			return $this->redirect('index');
-		}
-		if (Yii::$app->request->isAjax) {
-			return $this->renderAjax('modal/create', [
-				'model' => $model
-			]);
-		}
-		return $this->render('create', [
-			'model' => $model
-		]);
-	}
 
 }
