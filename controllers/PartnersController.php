b171084b33f8a36a02530c8351a914568d3445cb
Pozitronik <antikillerxxl@gmail.com>
2021-05-18 20:41:26 +0300

    Наследование от DefaultController

--- controllers/PartnersController.php
+++ controllers/PartnersController.php
@@ -3,73 +3,25 @@ declare(strict_types = 1);
 
 namespace app\controllers;
 
+use app\models\core\prototypes\DefaultController;
 use app\models\partners\{PartnersSearch, Partners};
 use pozitronik\core\traits\ControllerTrait;
-use pozitronik\sys_exceptions\models\LoggedException;
-use Throwable, Yii;
-use yii\web\{Controller, NotFoundHttpException, Response};
-use kartik\form\ActiveForm;
 
 /**
  * Class PartnersController
  * @package app\controllers
  */
-class PartnersController extends Controller
+class PartnersController extends DefaultController
 {
 	use ControllerTrait;
 
-	/**
-	 * Все партнеры
-	 * @return string
-	 */
-	public function actionIndex(): string
+	public string $modelClass = Partners::class;
+
+	public string $modelSearchClass = PartnersSearch::class;
+
+	public function getViewPath(): string
 	{
-		$params = Yii::$app->request->queryParams;
-		$searchModel = new PartnersSearch();
-		$dataProvider = $searchModel->search($params);
-		return $this->render('index', compact('searchModel', 'dataProvider'));
+		return '@app/views/partners';
 	}
 
-	/**
-	 * Создание нового партнера
-	 * @return string|Response|array
-	 * @throws Throwable
-	 */
-	public function actionCreate()
-	{
-		$model = new Partners();
-		// AJAX валидация на сервере с сохранением клиентской, в частности уникальный ИНН
-		if (Yii::$app->request->isAjax && $model->load(Yii::$app->request->post())) {
-			$errors = ActiveForm::validate($model);
-			if ($errors !== []) {
-				Yii::$app->response->format = Response::FORMAT_JSON;
-				return $errors;
-			}
-		}
-		return $model->createModelFromPost() ? $this->redirect('index') : $this->renderAjax('modal/create', compact('model'));
-	}
-
-	/**
-	 * Редактирование партнера
-	 * @param int $id
-	 * @return string|Response|array
-	 * @throws LoggedException
-	 * @throws Throwable
-	 */
-	public function actionEdit(int $id)
-	{
-		$model = Partners::findOne($id);
-		if ($model === null) {
-			throw new LoggedException(new NotFoundHttpException());
-		}
-		// AJAX валидация на сервере с сохранением клиентской, в частности уникальный ИНН
-		if (Yii::$app->request->isAjax && $model->load(Yii::$app->request->post())) {
-			$errors = ActiveForm::validate($model);
-			if ($errors !== []) {
-				Yii::$app->response->format = Response::FORMAT_JSON;
-				return $errors;
-			}
-		}
-		return $model->updateModelFromPost() ? $this->redirect('index') : $this->renderAjax('modal/edit', compact('model'));
-	}
 }
