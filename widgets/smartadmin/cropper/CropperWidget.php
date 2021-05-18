0df494abbe5ffd5f00f4c899fe1f3c19009eb853
Alexei Lapin <lapin.alx@gmail.com>
2021-05-18 17:28:43 +0300

    cropper: widget

--- widgets/smartadmin/cropper/CropperWidget.php
+++ widgets/smartadmin/cropper/CropperWidget.php
@@ -12,11 +12,22 @@ use yii\base\Widget as YiiBaseWidget;
 class CropperWidget extends YiiBaseWidget
 {
 	/**
-	 * @var array настройки, используемые для настройки Cropper
+	 * @var array настройки, используемые для инициализации Cropper
 	 * @see https://github.com/fengyuanchen/cropperjs
 	 */
 	public array $pluginOptions = [];
-
+	/**
+	 * @var string id <img> для подгрузки фото в кроппер
+	 */
+	public string $imageId = 'cropper__user-logo_img';
+	/**
+	 * @var string id файлового input'а
+	 */
+	public string $cropperUploadInputId = 'cropper__upload-input';
+	/**
+	 * @var string id элемента, который будет триггерить кроппинг изображения и его отправку на сервак
+	 */
+	public string $cropperCropElementId = 'cropper__crop';
 	/**
 	 * @var string|null ID модалки, на случай если кроппер завернут в неё.
 	 * Требуется для корректной инициализации кроппера.
@@ -31,22 +42,16 @@ class CropperWidget extends YiiBaseWidget
 
 	public function run(): string
 	{
-		$options = json_encode($this->pluginOptions);
+		$options = json_encode([
+			'imageId' => "#$this->imageId",
+			'modalId' => $this->modalId ? "#$this->modalId" : null,
+			'cropperUploadInputId' => "#$this->cropperUploadInputId",
+			'cropperCropElementId' => "#$this->cropperCropElementId",
+			'pluginOptions' => $this->pluginOptions
+		]);
 
-		if ($this->modalId) {
-			$jsInit = <<<JS
-$('$this->modalId').on('shown.bs.modal', function () { 
-    const image = $('#cropper__user-logo_img');
-    if (image.data('cropper') === undefined) {
-        image.cropper($options);
-    } 
-});
-JS;
-			$this->view->registerJs($jsInit);
-		} else {
-			$this->view->registerJs("$('#cropper__user-logo_img').cropper({$options});");
-		}
+		$this->view->registerJs("cropperInitConfig.init({$options})");
 
-		return $this->render('main');
+		return $this->render('main', ['widget' => $this]);
 	}
 }
