2c0ee09a89f5e43d2959c6cb2c30ceac5f4d3559
Pozitronik <antikillerxxl@gmail.com>
2021-05-19 10:36:13 +0300

    Вью для симкарты

---
+++ views/simcard/index.php
@@ -0,0 +1,72 @@
+<?php /** @noinspection PhpPossiblePolymorphicInvocationInspection */
+declare(strict_types = 1);
+
+/**
+ * @var View $this
+ * @var SimCardSearch $searchModel
+ * @var string $modelName
+ * @var ControllerTrait $controller
+ * @var ActiveDataProvider $dataProvider
+ */
+
+use app\assets\ModalHelperAsset;
+use app\models\core\TemporaryHelper;
+use app\models\product\SimCard;
+use app\models\product\SimCardSearch;
+use kartik\grid\GridView;
+use pozitronik\core\traits\ControllerTrait;
+use pozitronik\grid_config\GridConfig;
+use pozitronik\helpers\Utils;
+use yii\data\ActiveDataProvider;
+use yii\grid\ActionColumn;
+use yii\helpers\Html;
+use yii\web\JsExpression;
+use yii\web\View;
+
+ModalHelperAsset::register($this);
+?>
+<?= GridConfig::widget([
+	'id' => "{$modelName}-index-grid",
+	'grid' => GridView::begin([
+		'dataProvider' => $dataProvider,
+		'filterModel' => $searchModel,
+		'panel' => [
+			'heading' => $this->title.(($dataProvider->totalCount > 0)?" (".Utils::pluralForm($dataProvider->totalCount, ['запись', 'записи', 'записей']).")":" (нет записей)"),
+		],
+		'summary' => null !== $searchModel?Html::a('Новая запись', $controller::to('create'), [
+			'class' => 'btn btn-success',
+			'onclick' => new JsExpression("AjaxModal('".$controller::to('create')."', '{$modelName}-modal-create-new');event.preventDefault();")
+		]):null,
+		'showOnEmpty' => true,
+		'emptyText' => Html::a('Новая запись', $controller::to('create'), [
+			'class' => 'btn btn-success',
+			'onclick' => new JsExpression("AjaxModal('".$controller::to('create')."', '{$modelName}-modal-create-new');event.preventDefault();")
+		]),
+		'export' => false,
+		'resizableColumns' => true,
+		'responsive' => true,
+		'columns' => array_merge(TemporaryHelper::GuessDataProviderColumns($dataProvider), [
+			[
+				'class' => ActionColumn::class,
+				'template' => '{edit}{view}{sell}',
+				'buttons' => [
+					'edit' => static function(string $url, SimCardSearch $model) use ($modelName) {
+						return Html::a('<i class="glyphicon glyphicon-edit"></i>', $url, [
+							'onclick' => new JsExpression("AjaxModal('$url', '{$modelName}-modal-edit-{$model->id}');event.preventDefault();")
+						]);
+					},
+					'view' => static function(string $url, SimCardSearch $model) use ($modelName) {
+						return Html::a('<i class="glyphicon glyphicon-eye-open"></i>', $url, [
+							'onclick' => new JsExpression("AjaxModal('$url', '{$modelName}-modal-view-{$model->id}');event.preventDefault();")
+						]);
+					},
+					'sell' => static function(string $url, SimCardSearch $model) use ($modelName) {
+						return Html::a('<i class="glyphicon glyphicon-usd"></i>', $url, [
+							'onclick' => new JsExpression("AjaxModal('$url', '{$modelName}-modal-view-{$model->id}');event.preventDefault();")
+						]);
+					},
+				],
+			]
+		]),
+	])
+]) ?>
