f6f8bb95530bc25e983451fbb2f308daeb00eae4
Pozitronik <antikillerxxl@gmail.com>
2021-05-19 11:26:32 +0300

    Не обязательно выносить в сабвью часть модалки, которая только в этой модалке и используется

--- views/users/modal/logo-cropper.php
+++ views/users/modal/logo-cropper.php
@@ -1,12 +1,28 @@
 <?php
 declare(strict_types = 1);
 
+use app\widgets\smartadmin\cropper\CropperWidget;
 use yii\bootstrap4\Modal;
 
 ?>
 
 <?php Modal::begin(['id' => 'cropperModal', 'title' => 'Фото профиля']) ?>
 
-<?= $this->render('../subviews/logo-cropper', ['modalId' => 'cropperModal']) ?>
+<?= CropperWidget::widget([
+	'modalId' => $modalId??null,
+	'pluginOptions' => [
+		'aspectRatio' => 1,
+		'viewMode' => 2,
+		'zoomable' => false,
+		'restore' => false,
+		'checkCrossOrigin' => false,
+		'checkOrientation' => false,
+		'highlight' => false,
+		'movable' => false,
+		'rotatable' => false,
+		'scalable' => false,
+		'toggleDragModeOnDblclick' => false,
+	]
+]) ?>
 
 <?php Modal::end();
