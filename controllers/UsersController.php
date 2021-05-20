e2fb24815f782802411184f5b2a545640a295693
Alexei Lapin <lapin.alx@gmail.com>
2021-05-20 12:21:18 +0300

    PRPL-5 user profile: live edit action
    (cherry picked from commit afacc5448dacd89d2001f69f141b13abb29302b0)

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -3,6 +3,7 @@ declare(strict_types = 1);
 
 namespace app\controllers;
 
+use app\models\core\prototypes\DefaultController;
 use app\models\sys\users\Users;
 use app\models\sys\users\UsersSearch;
 use pozitronik\core\traits\ControllerTrait;
@@ -11,96 +12,49 @@ use Throwable;
 use Yii;
 use yii\db\Exception;
 use yii\filters\ContentNegotiator;
-use yii\web\Controller;
+use yii\helpers\ArrayHelper;
 use yii\web\NotFoundHttpException;
 use yii\web\Response;
 
 /**
  * Class UsersController
  */
-class UsersController extends Controller {
+class UsersController extends DefaultController
+{
 	use ControllerTrait;
+
+	/**
+	 * Поисковая модель пользователя
+	 * @var string
+	 */
+	public string $modelSearchClass = UsersSearch::class;
+
+	/**
+	 * Модель пользователя
+	 * @var string
+	 */
+	public string $modelClass = Users::class;
+
+	/**
+	 * Переопределим базовую директорию views
+	 * @return string
+	 */
+	public function getViewPath(): string
+	{
+		return '@app/views/users';
+	}
 
 	public function behaviors(): array
 	{
-		return [
-			'contentNegotiator' => [
-				'class' => ContentNegotiator::class,
-				'only' => ['logo-upload'],
+		return ArrayHelper::merge(parent::behaviors(), [
+			[
+				'class'   => ContentNegotiator::class,
+				'only'    => ['logo-upload'],
 				'formats' => [
 					'application/json' => Response::FORMAT_JSON,
 				],
 			],
-		];
-	}
-
-	/**
-	 * Основной список пользователей
-	 * @return string
-	 * @throws Throwable
-	 */
-	public function actionIndex():string {
-		$params = Yii::$app->request->queryParams;
-		$searchModel = new UsersSearch();
-		$allowedGroups = [];
-
-		$dataProvider = $searchModel->search($params, $allowedGroups);
-
-		return $this->render('index', compact('searchModel', 'dataProvider'));
-	}
-
-	/**
-	 * @param int $id
-	 * @return string|Response
-	 * @throws Throwable
-	 */
-	public function actionEdit(int $id) {
-		if (null === $user = Users::findOne($id)) {
-			throw new LoggedException(new NotFoundHttpException());
-		}
-
-		if ($user->updateModelFromPost()) return $this->redirect('index');
-
-		if (Yii::$app->request->isAjax) {
-			return $this->renderAjax('modal/edit', [
-				'model' => $user
-			]);
-		}
-		return $this->render('edit', [
-			'model' => $user
 		]);
-	}
-
-	/**
-	 * @return string|Response
-	 * @throws Throwable
-	 */
-	public function actionCreate() {
-		$user = new Users();
-		if ($user->createModelFromPost()) {
-			return $this->redirect('index');
-		}
-		if (Yii::$app->request->isAjax) {
-			return $this->renderAjax('modal/create', [
-				'model' => $user
-			]);
-		}
-		return $this->render('create', [
-			'model' => $user
-		]);
-	}
-
-	/**
-	 * @param int $id
-	 * @return Response
-	 * @throws Throwable
-	 */
-	public function actionDelete(int $id):Response {
-		if (null === $user = Users::findOne($id)) {
-			throw new LoggedException(new NotFoundHttpException());
-		}
-		$user->safeDelete();
-		return $this->redirect('index');
 	}
 
 	/**
@@ -109,8 +63,10 @@ class UsersController extends Controller {
 	 * @return string|null
 	 * @throws Throwable
 	 */
-	public function actionProfile(int $id):?string {
-		if (null === $user = Users::findOne($id)) {
+	public function actionProfile(int $id = null): ?string
+	{
+		$user = $id ? Users::findIdentity($id) : Users::Current();
+		if (null === $user) {
 			throw new LoggedException(new NotFoundHttpException());
 		}
 		if (Yii::$app->request->isAjax) {
@@ -130,7 +86,8 @@ class UsersController extends Controller {
 	 * @throws Throwable
 	 * @throws Exception
 	 */
-	public function actionUpdatePassword(int $id) {
+	public function actionUpdatePassword(int $id)
+	{
 		if (null === $user = Users::findOne($id)) {
 			throw new LoggedException(new NotFoundHttpException());
 		}
