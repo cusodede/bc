cda6277924a64b42dad470c25f9001bfdc08d35e
bm <PSuares@beeline.ru>
2021-05-21 10:37:30 +0300

    issue #16 - created migration ref_reward_status

---
+++ migrations/m210521_073447_RefRewardStatus.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types = 1);
+use pozitronik\references\models\CustomisableReferenceMigration;
+
+/**
+ * Class m210521_073447_RefRewardStatus
+ */
+class m210521_073447_RefRewardStatus extends CustomisableReferenceMigration {
+
+	public string $table_name = "ref_reward_status";
+
+}
