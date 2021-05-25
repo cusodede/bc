b6fbfc0b161b6a5de139d0b872399172374d9623
Pozitronik <antikillerxxl@gmail.com>
2021-05-25 15:22:08 +0300

    Замена glyphicon на font-awesome (bs3=>bs4)

--- views/sellers/index.php
+++ views/sellers/index.php
@@ -51,12 +51,12 @@ ModalHelperAsset::register($this);
 				'template' => '{edit}{view}',
 				'buttons' => [
 					'edit' => static function(string $url, SellersSearch $model) use ($modelName):string {
-						return Html::a('<i class="glyphicon glyphicon-edit"></i>', $url, [
+						return Html::a('<i class="fa fa-edit"></i>', $url, [
 							'onclick' => new JsExpression("AjaxModal('$url', '{$modelName}-modal-edit-{$model->id}');event.preventDefault();")
 						]);
 					},
 					'view' => static function(string $url, SellersSearch $model) use ($modelName):string {
-						return Html::a('<i class="glyphicon glyphicon-eye-open"></i>', $url, [
+						return Html::a('<i class="fa fa-eye"></i>', $url, [
 							'onclick' => new JsExpression("AjaxModal('$url', '{$modelName}-modal-view-{$model->id}');event.preventDefault();")
 						]);
 					},
--- views/stores/index.php
+++ views/stores/index.php
@@ -52,12 +52,12 @@ ModalHelperAsset::register($this);
 				'template' => '{edit}{view}',
 				'buttons' => [
 					'edit' => static function(string $url, StoresSearch $model) use ($modelName) {
-						return Html::a('<i class="glyphicon glyphicon-edit"></i>', $url, [
+						return Html::a('<i class="fa fa-edit"></i>', $url, [
 							'onclick' => new JsExpression("AjaxModal('$url', '{$modelName}-modal-edit-{$model->id}');event.preventDefault();")
 						]);
 					},
 					'view' => static function(string $url, StoresSearch $model) use ($modelName) {
-						return Html::a('<i class="glyphicon glyphicon-eye-open"></i>', $url, [
+						return Html::a('<i class="fa fa-eye"></i>', $url, [
 							'onclick' => new JsExpression("AjaxModal('$url', '{$modelName}-modal-view-{$model->id}');event.preventDefault();")
 						]);
 					},
