64de9c2c378d3f71cdcef41f26598d0d1fbcfda1
Pozitronik <antikillerxxl@gmail.com>
2021-05-25 10:53:05 +0300

    Переделал на Html

--- widgets/smartadmin/sidebar/views/subviews/logo.php
+++ widgets/smartadmin/sidebar/views/subviews/logo.php
@@ -1,14 +1,20 @@
 <?php
 declare(strict_types = 1);
+use yii\bootstrap4\Html;
 
 ?>
 
 <div class="page-logo">
-	<a href="/" class="page-logo-link press-scale-down d-flex align-items-center position-relative">
-		<img src="/img/theme/logo-bee.png" alt="<?= Yii::$app->name ?>" aria-roledescription="logo">
-		<span class="page-logo-text mr-1">
-			<?= Yii::$app->name ?>
-		</span>
-		<span class="position-absolute text-white opacity-50 small pos-top pos-right mr-2 mt-n2"></span>
-	</a>
+	<?= Html::a(
+		Html::img('/img/theme/logo-bee.png', [/*todo: вынести в стили*/
+			'aria-roledescription' => 'logo',
+			'alt' => Yii::$app->name
+		]).
+		Html::tag('span', Yii::$app->name, ['class' => 'page-logo-text mr-1']).
+		Html::tag('span', '', ['class' => 'position-absolute text-white opacity-50 small pos-top pos-right mr-2 mt-n2']),
+		'/',
+		[
+			'class' => 'page-logo-link press-scale-down d-flex align-items-center position-relative'
+		]
+	) ?>
 </div>
