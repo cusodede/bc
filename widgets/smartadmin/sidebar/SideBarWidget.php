9fa22712aeb5ee23bdec8b444ffcd41667d7c6df
Pozitronik <antikillerxxl@gmail.com>
2021-05-26 19:27:35 +0300

    Добавлен обработчик флага visible: любой пункт меню с 'visible' => false не будет показан

--- widgets/smartadmin/sidebar/SideBarWidget.php
+++ widgets/smartadmin/sidebar/SideBarWidget.php
@@ -3,7 +3,9 @@ declare(strict_types = 1);
 
 namespace app\widgets\smartadmin\sidebar;
 
+use Exception;
 use yii\base\Widget as YiiBaseWidget;
+use yii\helpers\ArrayHelper;
 use yii\widgets\Menu;
 
 /**
@@ -20,6 +22,19 @@ class SideBarWidget extends YiiBaseWidget {
 	public array $items = [];
 
 	public function run():string {
+		self::prepareItems($this->items);
 		return $this->render('main', ['items' => $this->items]);
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	private static function prepareItems(array &$items):void {
+		$items = array_filter($items, function(array $item) {
+			if ((null !== $subItems = ArrayHelper::getValue($item, 'items')) && is_array($subItems)) {
+				self::prepareItems($item['items']);
+			}
+			return (ArrayHelper::getValue($item, 'visible', true));
+		}, ARRAY_FILTER_USE_BOTH);
 	}
 }
