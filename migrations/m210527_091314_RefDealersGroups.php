d6fe126b05ab8a08a3950fabcb38c15387a52b6a
Pozitronik <antikillerxxl@gmail.com>
2021-05-27 12:15:56 +0300

    Миграции на таблицы справочников

---
+++ migrations/m210527_091314_RefDealersGroups.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types = 1);
+use pozitronik\references\models\CustomisableReferenceMigration;
+
+/**
+ * Class m210527_091314_RefDealersGroups
+ */
+class m210527_091314_RefDealersGroups extends CustomisableReferenceMigration {
+	public string $table_name = "ref_dealers_groups";
+}
