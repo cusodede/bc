8a023ad54f54cbd592f21a9fd25b5f5767159bc3
Pozitronik <antikillerxxl@gmail.com>
2021-05-27 11:36:07 +0300

    Остаточное: не везде заменились стили подвалов панелей

--- views/permissions/create.php
+++ views/permissions/create.php
@@ -20,7 +20,7 @@ use yii\web\View;
 		<div class="panel-content">
 			<?= $this->render('subviews/editPanelBody', compact('model', 'form')) ?>
 		</div>
-		<div class="panel-footer">
+		<div class="panel-container">
 			<?= $this->render('subviews/editPanelFooter', compact('model', 'form')) ?>
 			<div class="clearfix"></div>
 		</div>
