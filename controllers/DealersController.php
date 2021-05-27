a70e79370f034bdb6477c23f428c3d5e5da78839
Pozitronik <antikillerxxl@gmail.com>
2021-05-27 21:46:07 +0300

    Правила импорта для дилеров

--- controllers/DealersController.php
+++ controllers/DealersController.php
@@ -4,6 +4,9 @@ declare(strict_types = 1);
 namespace app\controllers;
 
 use app\models\core\prototypes\DefaultController;
+use app\models\dealers\active_record\references\RefBranches;
+use app\models\dealers\active_record\references\RefDealersGroups;
+use app\models\dealers\active_record\references\RefDealersTypes;
 use app\models\dealers\Dealers;
 use app\models\dealers\DealersSearch;
 
@@ -15,10 +18,42 @@ class DealersController extends DefaultController {
 	public string $modelClass = Dealers::class;
 	public string $modelSearchClass = DealersSearch::class;
 
+	public array $mappingRules = [
+		/*номер столбца => правило сопоставления*/
+		0 => null, /*skip*/
+		1 => [
+			'attribute' => 'group',//куда вставить значение
+			'foreign' => [//какая-то связь
+				'class' => RefDealersGroups::class,//с чем
+				'attribute' => 'name',//в какое поле вставить значение
+				'key' => 'id'//что получить
+			],
+		],
+		2 => [
+			'attribute' => 'branch',
+			'foreign' => [
+				'class' => RefBranches::class,
+				'attribute' => 'name',
+				'key' => 'id'
+			]
+		],
+		3 => [
+			'attribute' => 'type',
+			'foreign' => [
+				'class' => RefDealersTypes::class,
+				'attribute' => 'name',
+				'key' => 'id']
+		],
+		4 => ['attribute' => 'name'],
+		5 => ['attribute' => 'code'],
+		6 => ['attribute' => 'client_code']
+	];
+
 	/**
 	 * @inheritDoc
 	 */
 	public function getViewPath():string {
 		return '@app/views/dealers';
 	}
+
 }
