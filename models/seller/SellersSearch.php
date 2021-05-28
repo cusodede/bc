43791cb0d26387195af7c93b656124ffc4a2a2ba
suares <pepe_botika69@yahoo.com>
2021-05-28 15:58:56 +0300

    issue #31 - modify search model for sellers

--- models/seller/SellersSearch.php
+++ models/seller/SellersSearch.php
@@ -10,18 +10,36 @@ use yii\data\ActiveDataProvider;
 /**
  * Class StoresSearch
  * @property null|string $store
+ *
+ * @property null|string $passportExplodedSeries
+ * @property null|string $passportExplodedNumber
  */
 final class SellersSearch extends SellersAR {
 
 	public ?string $store = null;
+	public ?string $passport = null;
 
 	/**
 	 * {@inheritdoc}
 	 */
 	public function rules():array {
 		return [
-			[['id', 'deleted'], 'integer'],
-			[['name', 'store'], 'string', 'max' => 255],
+			[
+				[
+					'id', 'deleted', 'is_wireman_shpd', 'is_resident', 'gender', 'name', 'surname', 'patronymic', 'email',
+					'passport', 'inn', 'snils', 'login', 'keyword', 'birthday', 'entry_date', 'store'
+				],
+				'filter',
+				'filter' => 'trim'
+			],
+			[['id', 'gender'], 'integer'],
+			[['deleted', 'is_wireman_shpd', 'is_resident'], 'boolean'],
+			['store', 'string', 'max' => 255],
+			[['birthday', 'entry_date'], 'date', 'format' => 'php:Y-m-d'],
+			[['login', 'keyword'], 'string', 'max' => 64],
+			[['name', 'surname', 'patronymic', 'email', 'passport'], 'string', 'max' => 128],
+			['inn', 'string', 'max' => 12],
+			['snils', 'string', 'max' => 14]
 		];
 	}
 
@@ -54,6 +72,22 @@ final class SellersSearch extends SellersAR {
 	private function filterData($query):void {
 		$query->andFilterWhere([self::tableName().'.id' => $this->id])
 			->andFilterWhere(['like', self::tableName().'.name', $this->name])
+			->andFilterWhere(['like', self::tableName().'.surname', $this->surname])
+			->andFilterWhere(['like', self::tableName().'.patronymic', $this->patronymic])
+			->andFilterWhere([self::tableName().'.gender' => $this->gender])
+			->andFilterWhere([self::tableName().'.birthday' => $this->birthday])
+			->andFilterWhere([self::tableName().'.login' => $this->login])
+			->andFilterWhere([self::tableName().'.email' => $this->email])
+			->andFilterWhere(['>=', self::tableName().'.create_date', $this->create_date])
+			->andFilterWhere(['>=', self::tableName().'.update_date', $this->update_date])
+			->andFilterWhere([self::tableName().'.passport_series' => $this->passportExplodedSeries])
+			->andFilterWhere([self::tableName().'.passport_number' => $this->passportExplodedNumber])
+			->andFilterWhere([self::tableName().'.entry_date' => $this->entry_date])
+			->andFilterWhere([self::tableName().'.inn' => $this->inn])
+			->andFilterWhere([self::tableName().'.snils' => $this->snils])
+			->andFilterWhere([self::tableName().'.keyword' => $this->keyword])
+			->andFilterWhere([self::tableName().'.is_resident' => $this->is_resident])
+			->andFilterWhere([self::tableName().'.is_wireman_shpd' => $this->is_wireman_shpd])
 			->andFilterWhere([self::tableName().'.deleted' => $this->deleted])
 			->andFilterWhere(['like', Stores::tableName().'.name', $this->store]);
 	}
@@ -66,5 +100,23 @@ final class SellersSearch extends SellersAR {
 			'defaultOrder' => ['id' => SORT_ASC],
 			'attributes' => ['id', 'name', 'deleted']
 		]);
+	}
+
+	/**
+	 * Searching passport field consists of series and number. This getter returns us series.
+	 * @return false|string[]
+	 */
+	public function getPassportExplodedSeries() {
+		$passportArray = explode(' ', $this->passport);
+		return array_shift($passportArray);
+	}
+
+	/**
+	 * Searching passport field consists of series and number. This getter returns us number.
+	 * @return mixed|string|null
+	 */
+	public function getPassportExplodedNumber() {
+		$passportArray = explode(' ', $this->passport);
+		return array_pop($passportArray);
 	}
 }
