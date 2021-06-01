ae85fcc7da915073aef368e817d93ddc0d521c08
Pozitronik <antikillerxxl@gmail.com>
2021-06-01 17:13:17 +0300

    Правильно именуемся

--- migrations/m210601_132233_rel_users_phones.php
+++ migrations/m210601_132233_rel_users_phones.php
@@ -6,7 +6,7 @@ use pozitronik\core\models\RelationMigration;
  * Class m210601_132233_rel_users_phones
  */
 class m210601_132233_rel_users_phones extends RelationMigration {
-	public string $table_name = 'rel_users_phones';
+	public string $table_name = 'relation_users_to_phones';
 	public string $first_key = 'user_id';
 	public string $second_key = 'phone_id';
 }
