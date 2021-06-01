3ab6030ac7ab1eb75c0f62c858e40bc7a80c7bee
Pozitronik <antikillerxxl@gmail.com>
2021-06-01 16:22:23 +0300

    Миграция на таблицу телефонных номеров

---
+++ migrations/m210601_131855_users_phones.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types = 1);
+use yii\db\Migration;
+
+/**
+ * Class m210601_131855_users_phones
+ */
+class m210601_131855_users_phones extends Migration {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeUp() {
+		$this->createTable('phones', [
+			'id' => $this->primaryKey(),
+			'phone' => $this->string()->notNull()->comment('Телефон'),
+			'create_date' => $this->dateTime()->notNull()->comment('Дата регистрации'),
+			'status' => $this->integer()->null()->comment('Статус'),
+			'deleted' => $this->boolean()->notNull()->defaultValue(0)
+		]);
+
+		$this->createIndex('phone', 'phones', 'phone');
+		$this->createIndex('status', 'phones', 'status');
+		$this->createIndex('deleted', 'phones', 'deleted');
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeDown() {
+		$this->dropTable('phones');
+	}
+
+}
