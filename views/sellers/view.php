a846190223488f263ade46e4543e8c2c588daa1f
suares <pepe_botika69@yahoo.com>
2021-06-01 11:17:53 +0300

    issue #31 - add fields to view.php for sellers

--- views/sellers/view.php
+++ views/sellers/view.php
@@ -6,8 +6,12 @@ declare(strict_types = 1);
  * @var Sellers $model
  */
 
+use app\controllers\UploadsController;
+use app\models\core\prototypes\ProjectConstants;
 use app\models\seller\Sellers;
 use pozitronik\widgets\BadgeWidget;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
 use yii\web\View;
 use yii\widgets\DetailView;
 use app\models\seller\active_record\SellersAR;
@@ -17,9 +21,17 @@ use app\models\seller\active_record\SellersAR;
 <?= DetailView::widget([
 	'model' => $model,
 	'attributes' => [
-		'id',
-		'name',
 		'create_date',
+		'update_date',
+		'surname',
+		'name',
+		'patronymic',
+		[
+			'attribute' => 'gender',
+			'value' => static function(Sellers $model) {
+				return ArrayHelper::getValue(ProjectConstants::GENDER, $model->gender);
+			},
+		],
 		[
 			'attribute' => 'stores',
 			'format' => 'raw',
@@ -30,6 +42,36 @@ use app\models\seller\active_record\SellersAR;
 				]);
 			}
 		],
-		'deleted:boolean'
+		'birthday',
+		'login',
+		'email',
+		'is_resident:boolean',
+		[
+			'attribute' => 'passport',
+			'value' => static function(Sellers $model) {
+				return "{$model->passport_series} {$model->passport_number}";
+			}
+		],
+		'passport_whom',
+		'passport_when',
+		'reg_address',
+		'entry_date',
+		'keyword',
+		'is_wireman_shpd:boolean',
+		'deleted:boolean',
+		[
+			'attribute' => 'uploadedFiles',
+			'format' => 'raw',
+			'value' => static function(Sellers $model):string {
+				$uploads = [];
+				foreach ($model->uploads as $upload) {
+					$uploads[] = Html::a(
+						'Скачать',
+						UploadsController::to('download', ['id' => $upload->id])
+					);
+				}
+				return implode('<br>', $uploads);
+			}
+		]
 	]
 ]) ?>
