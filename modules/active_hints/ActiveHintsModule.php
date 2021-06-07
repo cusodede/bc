34b59f937b6dc0feffac4072d300c0b487e64213
Pozitronik <antikillerxxl@gmail.com>
2021-06-07 15:12:58 +0300

    Фикс неймспейса

--- modules/active_hints/ActiveHintsModule.php
+++ modules/active_hints/ActiveHintsModule.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types = 1);
 
-namespace app\modules\status;
+namespace app\modules\active_hints;
 
 use pozitronik\core\helpers\ModuleHelper;
 use pozitronik\core\traits\ModuleExtended;
