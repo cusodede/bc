fbb1429440453274f161d6c7caed8376ba1d2e5c
Pozitronik <antikillerxxl@gmail.com>
2021-06-07 20:54:21 +0300

    ActiveField для перекрытия стандарного

---
+++ modules/active_hints/models/ActiveField.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\active_hints\models;
+
+use app\modules\active_hints\ActiveHintsModule;
+use app\modules\active_hints\widgets\active_hints\ActiveHints;
+use Exception;
+use Throwable;
+use yii\bootstrap4\ActiveField as ActiveFieldBs4;
+
+/**
+ * Class ActiveField
+ */
+class ActiveField extends ActiveFieldBs4 {
+
+	/**
+	 * @var bool whether to render the active hint. Default is `true`.
+	 */
+	public bool $enableActiveHint = true;
+	/**
+	 * @var bool whether to render the active hint editor or just show the hint contents. Default is `true`.
+	 */
+	public $editable = true;
+
+	public $template = "{label}{activeHint}\n{input}\n{error}";
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function render($content = null) {
+		if ($content === null) {
+			$this->parts['{activeHint}'] = $this->getActiveHintContents();
+		}
+		return parent::render($content);
+	}
+
+	/**
+	 * @return string
+	 * @throws Exception
+	 * @throws Throwable
+	 */
+	private function getActiveHintContents() {
+		return ActiveHints::widget([
+			'model' => $this->model,
+			'attribute' => $this->attribute,
+			'editable' => $this->enableActiveHint,
+			'editAction' => ActiveHintsModule::to('editHint')
+		]);
+	}
+
+}
