ee80775dcd44fcb21ece91bf0d442450bede463f
Pozitronik <antikillerxxl@gmail.com>
2021-06-09 09:47:58 +0300

    Редактор и подсказка могут существовать отдельно

--- modules/active_hints/widgets/active_hints/ActiveHints.php
+++ modules/active_hints/widgets/active_hints/ActiveHints.php
@@ -3,11 +3,14 @@ declare(strict_types = 1);
 
 namespace app\modules\active_hints\widgets\active_hints;
 
+use app\assets\AppAsset;
+use app\assets\ModalHelperAsset;
 use app\modules\active_hints\ActiveHintsModule;
 use app\modules\active_hints\models\ActiveStorage;
 use kartik\popover\PopoverX;
 use yii\base\Model;
 use yii\base\Widget;
+use yii\bootstrap4\BootstrapAsset;
 use yii\helpers\Html;
 use yii\web\JsExpression;
 
@@ -37,6 +40,7 @@ class ActiveHints extends Widget {
 	public function init():void {
 		parent::init();
 		ActiveHintsAssets::register($this->getView());
+		if ($this->editable) ModalHelperAsset::register($this->getView());
 		$this->_record = ActiveStorage::findActiveAttribute($this->model, $this->attribute);
 		$this->editAction = $this->editAction??ActiveHintsModule::to(['default/set-hint', 'model' => $this->_record->model, 'attribute' => $this->_record->attribute]);
 		$this->_for = "{$this->model->formName()}-{$this->attribute}";
@@ -47,20 +51,18 @@ class ActiveHints extends Widget {
 	 */
 	public function run():string {
 		$output = '';
-
-
-
 		$this->_toggleButtonClass['hint'] = (null === $this->_record->content)?'no-hint':'has-hint';
 		if ($this->editable) {//Если у хинта нет контента, то показываем его только для редактирования
-			$output = Html::button('<i class="fa fa-question-circle"></i>', [
+			$output = Html::button('<i class="fa fa-comment-alt-edit"></i>', [
 				'class' => $this->toggleButtonClass,
 				'onclick' => new JsExpression("AjaxModal('$this->editAction', '{$this->_for}-modal');event.preventDefault();"),
 			]);
-		} elseif (null !== $this->_record->content) {//Если у хинта нет контента, то показываем его только для редактирования
-			$output = PopoverX::widget([
+		}
+		if (null !== $this->_record->content) {
+			$output .= PopoverX::widget([
 				'content' => $this->_record->content,
 				'header' => $this->_record->header??'Подсказка',
-				'placement' => $this->_record->placement??PopoverX::ALIGN_RIGHT,
+				'placement' => $this->_record->placement??ActiveHintsModule::getConfigParameter('defaultPlacement'),
 				'toggleButton' => $this->toggleButton??['label' => '<i class="fa fa-question-circle"></i>', 'class' => $this->toggleButtonClass],
 				'footer' => $this->footer
 			]);
