c91c02b954bc95d2f9e4a78b568af2deba7311b9
Pozitronik <antikillerxxl@gmail.com>
2021-06-09 10:11:32 +0300

    Добавлены параметры и их описание

--- modules/active_hints/ActiveHintsModule.php
+++ modules/active_hints/ActiveHintsModule.php
@@ -3,6 +3,7 @@ declare(strict_types = 1);
 
 namespace app\modules\active_hints;
 
+use kartik\popover\PopoverX;
 use pozitronik\core\helpers\ModuleHelper;
 use pozitronik\core\traits\ModuleExtended;
 use pozitronik\helpers\ArrayHelper;
@@ -16,14 +17,25 @@ use yii\base\Module;
 class ActiveHintsModule extends Module {
 	use ModuleExtended;
 
+	/**
+	 * Параметры `enabled` и `editable` можно задать замыканием вида
+	 * function (IdentityInterface $identity):bool {
+	 *
+	 * }
+	 */
 	public const DEFAULT_CONFIG = [
-		'tableName' => 'sys_active_hints'
+		'tableName' => 'sys_active_hints',
+		'defaultPlacement' => PopoverX::ALIGN_AUTO,
+		'template' => "{label}{activeHint}\n{input}\n{error}",
+		'enabled' => true,
+		'editable' => false
 	];
 
 	/**
 	 * @param string $name
-	 * @throws Throwable
+	 * @return mixed
 	 * @throws InvalidConfigException
+	 * @throws Throwable
 	 */
 	public static function getConfigParameter(string $name) {
 		return ArrayHelper::getValue(ModuleHelper::params(self::class), $name, ArrayHelper::getValue(self::DEFAULT_CONFIG, $name));
