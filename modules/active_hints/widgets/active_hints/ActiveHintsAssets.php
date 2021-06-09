61365cf17782e72e64f33b7868d7393f98f8922e
Pozitronik <antikillerxxl@gmail.com>
2021-06-09 10:44:18 +0300

    forceCopy config

--- modules/active_hints/widgets/active_hints/ActiveHintsAssets.php
+++ modules/active_hints/widgets/active_hints/ActiveHintsAssets.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 namespace app\modules\active_hints\widgets\active_hints;
 
 use app\assets\AppAsset;
+use pozitronik\sys_options\models\SysOptions;
 use yii\web\AssetBundle;
 
 /**
@@ -16,8 +17,13 @@ class ActiveHintsAssets extends AssetBundle {
 	public function init():void {
 		$this->sourcePath = __DIR__.'/assets';
 		$this->css = ['css/active_hints.css'];
+//		$this->js = ['js/active_hints.js'];
 		$this->depends = [
 			AppAsset::class
+		];
+
+		$this->publishOptions = [
+			'forceCopy' => SysOptions::getStatic('ASSETS_PUBLISHOPTIONS_FORCECOPY', false)
 		];
 		parent::init();
 	}
