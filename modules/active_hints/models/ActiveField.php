e724cf7ab7f0c0aded0681b869fd673b53aaf4c7
Pozitronik <antikillerxxl@gmail.com>
2021-06-09 10:12:56 +0300

    Параметры подсасываются из конфига

--- modules/active_hints/models/ActiveField.php
+++ modules/active_hints/models/ActiveField.php
@@ -7,6 +7,7 @@ use app\modules\active_hints\ActiveHintsModule;
 use app\modules\active_hints\widgets\active_hints\ActiveHints;
 use Exception;
 use Throwable;
+use Yii;
 use yii\bootstrap4\ActiveField as ActiveFieldBs4;
 
 /**
@@ -17,19 +18,28 @@ class ActiveField extends ActiveFieldBs4 {
 	/**
 	 * @var bool whether to render the active hint. Default is `true`.
 	 */
-	public bool $enableActiveHint = true;
+	public bool $enabled = true;
 	/**
 	 * @var bool whether to render the active hint editor or just show the hint contents. Default is `true`.
 	 */
-	public $editable = true;
+	public $editable = false;
 
 	public $template = "{label}{activeHint}\n{input}\n{error}";
+
+	public function init():void {
+		parent::init();
+		$this->template = ActiveHintsModule::getConfigParameter('template');
+		$this->enabled = ActiveHintsModule::getConfigParameter('enabled');
+		if (is_callable($this->enabled)) $this->enabled = call_user_func($this->enabled, Yii::$app->user->identity);
+		$this->editable = ActiveHintsModule::getConfigParameter('editable');
+		if (is_callable($this->editable)) $this->editable = call_user_func($this->editable, Yii::$app->user->identity);
+	}
 
 	/**
 	 * {@inheritdoc}
 	 */
-	public function render($content = null) {
-		if ($content === null) {
+	public function render($content = null):string {
+		if (null === $content && $this->enabled) {
 			$this->parts['{activeHint}'] = $this->getActiveHintContents();
 		}
 		return parent::render($content);
@@ -40,11 +50,11 @@ class ActiveField extends ActiveFieldBs4 {
 	 * @throws Exception
 	 * @throws Throwable
 	 */
-	private function getActiveHintContents() {
+	private function getActiveHintContents():string {
 		return ActiveHints::widget([
 			'model' => $this->model,
 			'attribute' => $this->attribute,
-			'editable' => $this->enableActiveHint,
+			'editable' => $this->editable,
 			'editAction' => ActiveHintsModule::to(['default/edit-hint', 'model' => get_class($this->model), 'attribute' => $this->attribute])
 		]);
 	}
