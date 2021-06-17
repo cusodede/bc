55a05821f21ce6e7cd65d36cee5e82bd881e04d6
Pozitronik <antikillerxxl@gmail.com>
2021-06-17 11:51:25 +0300

    Случайно гранату

--- models/products/active_record/SimCardAR.php
+++ models/products/active_record/SimCardAR.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types = 1);
 
-namespace app\models\product\active_record;
+namespace app\models\products\active_record;
 
 use app\models\core\prototypes\ActiveRecordTrait;
 use yii\db\ActiveRecord;
