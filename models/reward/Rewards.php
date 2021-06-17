7927090714a820c0def85f575b7b1a596fc07f20
Pozitronik <antikillerxxl@gmail.com>
2021-06-17 11:47:05 +0300

    public const

--- models/reward/Rewards.php
+++ models/reward/Rewards.php
@@ -14,17 +14,17 @@ use app\models\reward\active_record\RewardsAR;
 class Rewards extends RewardsAR {
 
 	/*Прототипирую причины начисления наград*/
-	const REASON_SALE_REGISTERED = 1;//За продажу товара
-	const REASON_SALE_CONFIRMED = 2;//Продажа активна
-	const REASON_FRAUD = 3;//Фродовая продажа
+	public const REASON_SALE_REGISTERED = 1;//За продажу товара
+	public const REASON_SALE_CONFIRMED = 2;//Продажа активна
+	public const REASON_FRAUD = 3;//Фродовая продажа
 
 	/*Статусы вознаграждений*/
-	const STATUS_APPLY = 1;//начислено (может быть выведено)
-	const STATUS_WAIT = 2;//начисление ждёт события, после которого будет начислено
-	const STATUS_HOLD = 3;//начисление заморожено (не знаем, сколько начислить, заподозрили фрод, etc)
-	const STATUS_ERROR = 4;//ошибочное начисление
+	public const STATUS_APPLY = 1;//начислено (может быть выведено)
+	public const STATUS_WAIT = 2;//начисление ждёт события, после которого будет начислено
+	public const STATUS_HOLD = 3;//начисление заморожено (не знаем, сколько начислить, заподозрили фрод, etc)
+	public const STATUS_ERROR = 4;//ошибочное начисление
 	/*Инициирующие операции*/
-	const OPERATION_SELL = 1;
+	public const OPERATION_SELL = 1;
 
 	/**
 	 * @param array $rewards
