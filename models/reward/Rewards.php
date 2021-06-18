f225930c428f7baeca5905c64039974ad53232ae
Pozitronik <antikillerxxl@gmail.com>
2021-06-18 17:24:58 +0300

    Конфигурация статусов операций + связь с конфигурацией операций

--- models/reward/Rewards.php
+++ models/reward/Rewards.php
@@ -5,6 +5,9 @@ namespace app\models\reward;
 
 use app\models\products\ProductsInterface;
 use app\models\reward\active_record\RewardsAR;
+use app\models\reward\config\RewardsOperationsConfig;
+use app\models\sys\users\Users;
+use Throwable;
 
 /**
  * Class Rewards
@@ -13,6 +16,7 @@ use app\models\reward\active_record\RewardsAR;
  * За что было вознаграждение? Я что-то продал
  *
  * @property null|ProductsInterface $relatedProducts Связанный проданный продукта
+ * @property-read null|RewardsOperationsConfig $relatedOperation Конфигурация статусов операций
  */
 class Rewards extends RewardsAR {
 
@@ -26,8 +30,61 @@ class Rewards extends RewardsAR {
 	public const STATUS_WAIT = 2;//начисление ждёт события, после которого будет начислено
 	public const STATUS_HOLD = 3;//начисление заморожено (не знаем, сколько начислить, заподозрили фрод, etc)
 	public const STATUS_ERROR = 4;//ошибочное начисление
-	/*Инициирующие операции*/
-	public const OPERATION_SELL = 1;
+	public const STATUS_PAID = 5;//выведен в налик
+	public const STATUS_DENY = 6;//Отказ в начислении
+
+	/**
+	 * @return array[]
+	 */
+	public static function status_config():array {
+		return [
+			self::STATUS_APPLY => [
+				'name' => 'Зачислен',
+				'initial' => false,
+				'finishing' => false,
+				'next' => [],
+				'allowed' => true
+
+			],
+			self::STATUS_WAIT => [
+				'name' => 'Ожидается',
+				'initial' => false,
+				'finishing' => false,
+				'next' => [],
+				'allowed' => static function(Rewards $model, Users $user):bool {
+					return true;
+				}
+			],
+			self::STATUS_HOLD => [
+				'name' => 'Заморожено',
+				'initial' => false,
+				'finishing' => false,
+				'next' => [],
+				'color' => '#ff0000'
+			],
+			self::STATUS_ERROR => [
+				'name' => 'Ошибочное начисление',
+				'initial' => false,
+				'finishing' => true,
+				'next' => [],
+				'color' => '#ff0000'
+			],
+			self::STATUS_PAID => [
+				'name' => 'Выведен',
+				'initial' => false,
+				'finishing' => true,
+				'next' => [],
+				'color' => '#ff0000'
+			],
+			self::STATUS_DENY => [
+				'name' => 'Отказано',
+				'initial' => false,
+				'finishing' => true,
+				'next' => [],
+				'color' => '#ff0000'
+			],
+		];
+	}
 
 	/**
 	 * @param self[] $rewards
@@ -50,24 +107,20 @@ class Rewards extends RewardsAR {
 	}
 
 	/**
-	 * @return string[]
-	 */
-	public static function statuses():array {
-		return [
-			self::STATUS_APPLY => 'Начислено',
-			self::STATUS_WAIT => 'Ожидается',
-			self::STATUS_HOLD => 'Заморожено',
-			self::STATUS_ERROR => 'Ошибочное начисление'
-		];
-	}
-
-	/**
 	 * Ну, допустим
 	 * @param ProductsInterface $product
 	 */
 	public function setRelatedProducts(ProductsInterface $product):void {
 		$this->product_id = $product->id;
 		$this->product_type = $product->type;
+	}
+
+	/**
+	 * @return null|RewardsOperationsConfig
+	 * @throws Throwable
+	 */
+	public function getRelatedOperation():?RewardsOperationsConfig {
+		return RewardsOperationsConfig::loadModel($this->operation);
 	}
 
 }
