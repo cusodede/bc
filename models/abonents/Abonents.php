48a0154e21e3ece346944faf3d7d2b36afd20c12
Alexei Lapin <lapin.alx@gmail.com>
2021-06-18 13:55:27 +0300

    Убрал модуль MyBeelineModule.php за ненадобностью (пока)

--- models/abonents/Abonents.php
+++ models/abonents/Abonents.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 namespace app\models\abonents;
 
 use app\models\abonents\active_record\Abonents as ActiveRecordAbonents;
+use app\models\products\Products;
 use yii\db\ActiveQuery;
 
 /**
@@ -15,10 +16,22 @@ use yii\db\ActiveQuery;
 class Abonents extends ActiveRecordAbonents
 {
 	/**
-	 * @return ActiveQuery
+	 * {@inheritdoc}
 	 */
 	public function getRelatedAbonentsToProducts(): ActiveQuery
 	{
 		return $this->hasMany(RelAbonentsToProducts::class, ['abonent_id' => 'id']);
+	}
+
+	/**
+	 * @return Products[] получение закрепленных за абонентом продуктов с фиксацией актуального статуса по каждому из них.
+	 */
+	public function getExistentProducts(): array
+	{
+		return array_map(static function ($abonentsToProduct) {
+			$abonentsToProduct->relatedProduct->actualStatus = $abonentsToProduct->relatedLastProductStatus;
+
+			return $abonentsToProduct->relatedProduct;
+		}, $this->relatedAbonentsToProducts);
 	}
 }
