f1a9b030495f13a5776f4e82ed1a29548fa3968d
suares <pepe_botika69@yahoo.com>
2021-06-18 10:18:18 +0300

    # 48 - add search for type, group, branch in dealers

--- models/dealers/DealersSearch.php
+++ models/dealers/DealersSearch.php
@@ -3,14 +3,18 @@ declare(strict_types = 1);
 
 namespace app\models\dealers;
 
+use app\models\branches\active_record\references\RefBranches;
 use app\models\dealers\active_record\DealersAR;
+use app\models\dealers\active_record\references\RefDealersGroups;
+use app\models\dealers\active_record\references\RefDealersTypes;
 use app\models\dealers\active_record\relations\RelDealersToStores;
 use app\models\managers\Managers;
 use app\models\store\Stores;
 use app\models\sys\users\Users;
+use pozitronik\core\models\LCQuery;
+use Throwable;
 use yii\data\ActiveDataProvider;
 use yii\helpers\ArrayHelper;
-use Throwable;
 
 /**
  * Class StoresSearch
@@ -26,7 +30,7 @@ final class DealersSearch extends DealersAR {
 	public function rules():array {
 		return [
 			[['id', 'client_code', 'code', 'name'], 'filter', 'filter' => 'trim'],
-			[['id', 'deleted'], 'integer'],
+			[['id', 'deleted', 'type', 'group', 'branch'], 'integer'],
 			[['name'], 'string', 'max' => 255],
 			[['code'], 'string', 'max' => 4],
 			[['client_code'], 'string', 'max' => 9]
@@ -39,7 +43,9 @@ final class DealersSearch extends DealersAR {
 	 * @throws Throwable
 	 */
 	public function search(array $params):ActiveDataProvider {
-		$query = self::find()->distinct()->active();
+		$query = self::find();
+		$query->joinWith(['relatedDealersToStores', 'refDealersGroups', 'refDealersTypes', 'refBranches']);
+		$this->initQuery($query);
 
 		$dataProvider = new ActiveDataProvider([
 			'query' => $query
@@ -47,7 +53,6 @@ final class DealersSearch extends DealersAR {
 
 		$this->setSort($dataProvider);
 		$this->load($params);
-		$query->joinWith(['relatedDealersToStores']);
 
 		if (!$this->validate()) return $dataProvider;
 
@@ -65,11 +70,29 @@ final class DealersSearch extends DealersAR {
 		$query->andFilterWhere([self::tableName().'.id' => $this->id])
 			->andFilterWhere(['like', self::tableName().'.name', $this->name])
 			->andFilterWhere([self::tableName().'.code' => $this->code])
+			->andFilterWhere([self::tableName().'.group' => $this->group])
+			->andFilterWhere([self::tableName().'.type' => $this->type])
+			->andFilterWhere([self::tableName().'.branch' => $this->branch])
 			->andFilterWhere([self::tableName().'.client_code' => $this->client_code])
 			->andFilterWhere([self::tableName().'.deleted' => $this->deleted])
 			->andFilterWhere(['like', Stores::tableName().'.name', $this->store]);
 
 		$this->filterDataByUser($query);
+	}
+
+	/**
+	 * @param LCQuery $query
+	 * @throws Throwable
+	 */
+	private function initQuery(LCQuery $query):void {
+		$query->select([
+			self::tableName().'.*',
+			RefDealersGroups::tableName().'.name AS groupName',
+			RefDealersTypes::tableName().'.name AS typeName',
+			RefBranches::tableName().'.name AS branchName',
+		])
+			->distinct()
+			->active();
 	}
 
 	/**
@@ -104,7 +127,25 @@ final class DealersSearch extends DealersAR {
 	private function setSort($dataProvider):void {
 		$dataProvider->setSort([
 			'defaultOrder' => ['id' => SORT_ASC],
-			'attributes' => ['id', 'name', 'code', 'client_code', 'deleted']
+			'attributes' => [
+				'id',
+				'name',
+				'code',
+				'client_code',
+				'typeName' => [
+					'asc' => [RefDealersTypes::tableName().'.name' => SORT_ASC],
+					'desc' => [RefDealersTypes::tableName().'.name' => SORT_DESC]
+				],
+				'groupName' => [
+					'asc' => [RefDealersGroups::tableName().'.name' => SORT_ASC],
+					'desc' => [RefDealersGroups::tableName().'.name' => SORT_DESC]
+				],
+				'branchName' => [
+					'asc' => [RefBranches::tableName().'.name' => SORT_ASC],
+					'desc' => [RefBranches::tableName().'.name' => SORT_DESC]
+				],
+				'deleted'
+			]
 		]);
 	}
 }
