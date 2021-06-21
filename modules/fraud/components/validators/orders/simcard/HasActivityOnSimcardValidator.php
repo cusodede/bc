0773932297b8415db9a46cd999b8f982dc7c99f6
Pozitronik <antikillerxxl@gmail.com>
2021-06-21 20:48:15 +0300

    1. Не забываем PHPDoc. Для методов, у которых при наследовании ничего не поменялось, фигачим @inheritDoc. 2. Ещё раз напомню: не отключаем инспекции. Я вижу, что тут у тебя прототипы какие-то, у которых параметры не используются. Ну и ок. Пусть инспектор на них ругается - это ж прототипы. А если ты заглушишь инспекцию, то потом не увидишь реального косяка, если будет.

--- modules/fraud/components/validators/orders/simcard/HasActivityOnSimcardValidator.php
+++ modules/fraud/components/validators/orders/simcard/HasActivityOnSimcardValidator.php
@@ -11,10 +11,17 @@ use app\modules\fraud\components\FraudValidator;
  * @package app\modules\fraud\components\validators\orders\simcard
  */
 class HasActivityOnSimcardValidator implements FraudValidator {
+
+	/**
+	 * @inheritDoc
+	 */
 	public function name():string {
 		return 'Есть активность по симке(звонки, интернет)';
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function validate(int $entityId):void {
 		$has = $this->hasIncomingCalls(5, 15) || $this->hasOutgoingCalls(5, 15) || $this->hasInternetTraffic(5, 15);
 		if (!$has) {
@@ -22,14 +29,29 @@ class HasActivityOnSimcardValidator implements FraudValidator {
 		}
 	}
 
+	/**
+	 * @param int $minutes
+	 * @param int $days
+	 * @return bool
+	 */
 	protected function hasIncomingCalls(int $minutes, int $days):bool {
 		return true;
 	}
 
+	/**
+	 * @param int $minutes
+	 * @param int $days
+	 * @return bool
+	 */
 	protected function hasOutgoingCalls(int $minutes, int $days):bool {
 		return false;
 	}
 
+	/**
+	 * @param int $MB
+	 * @param int $days
+	 * @return bool
+	 */
 	protected function hasInternetTraffic(int $MB, int $days):bool {
 		return true;
 	}
