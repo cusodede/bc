e83b7f4d5f353f6de72015b2a668b7a1c70fe01b
Pozitronik <antikillerxxl@gmail.com>
2021-06-24 11:11:20 +0300

    app\models\core\prototypes\DefaultController переехал в app\components\web\DefaultController

--- components/web/DefaultController.php
+++ components/web/DefaultController.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types = 1);
 
-namespace app\models\core\prototypes;
+namespace app\components\web;
 
 use app\components\db\ActiveRecordTrait;
 use app\models\sys\permissions\traits\ControllerPermissionsTrait;
--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -3,7 +3,7 @@ declare(strict_types = 1);
 
 namespace app\controllers;
 
-use app\models\core\prototypes\DefaultController;
+use app\components\web\DefaultController;
 use app\models\sys\users\Users;
 use app\models\sys\users\UsersSearch;
 use Throwable;
