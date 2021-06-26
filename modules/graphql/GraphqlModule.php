11baa49b5698ef8480ed196b05dfe4832eee8bac
Pozitronik <antikillerxxl@gmail.com>
2021-06-26 12:03:48 +0300

    Trait namespace changed

--- modules/graphql/GraphqlModule.php
+++ modules/graphql/GraphqlModule.php
@@ -3,7 +3,7 @@ declare(strict_types = 1);
 
 namespace app\modules\graphql;
 
-use pozitronik\core\traits\ModuleExtended;
+use pozitronik\traits\traits\ModuleTrait;
 use yii\base\Module;
 
 /**
@@ -11,5 +11,5 @@ use yii\base\Module;
  * @package app\modules\graphql
  */
 class GraphqlModule extends Module {
-	use ModuleExtended;
+	use ModuleTrait;
 }
