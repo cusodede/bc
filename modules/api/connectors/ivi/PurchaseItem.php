07a06f7dd8c6ced314b0e172c83d196b5c83f97a
Alexei Lapin <lapin.alx@gmail.com>
2021-06-30 11:19:25 +0300

    Добавлены геттеры для получения информации о подписках ivi

--- modules/api/connectors/ivi/PurchaseItem.php
+++ modules/api/connectors/ivi/PurchaseItem.php
@@ -3,6 +3,9 @@ declare(strict_types = 1);
 
 namespace app\modules\api\connectors\ivi;
 
+use Exception;
+use yii\helpers\ArrayHelper;
+
 /**
  * Class PurchaseItem
  * @package app\modules\api\connectors\ivi
@@ -14,5 +17,23 @@ class PurchaseItem
 	public function __construct(array $data)
 	{
 		$this->_data = $data;
+	}
+
+	/**
+	 * @return int|null
+	 * @throws Exception
+	 */
+	public function getId(): ?int
+	{
+		return ArrayHelper::getValue($this->_data, 'id');
+	}
+
+	/**
+	 * @return string|null
+	 * @throws Exception
+	 */
+	public function getExpireDate(): ?string
+	{
+		return ArrayHelper::getValue($this->_data, 'finish_time');
 	}
 }
