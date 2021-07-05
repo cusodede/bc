fecc14e3e43f7e8132def62287f68ded835c4a17
Alexei Lapin <lapin.alx@gmail.com>
2021-07-05 19:24:13 +0300

    Добавлена модель релейшенов для связи токенов между собой

---
+++ migrations/m210705_140200_add_relation_to_users_token_to_tokens.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types = 1);
+use pozitronik\relations\models\RelationMigration;
+
+/**
+* Class m210705_140200_add_relation_to_users_token_to_tokens
+*/
+class m210705_140200_add_relation_to_users_token_to_tokens extends RelationMigration {
+	public string $table_name = 'sys_relation_users_tokens_to_tokens';
+	public string $first_key = 'parent_id';
+	public string $second_key = 'child_id';
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeUp() {
+		parent::safeUp();
+		$this->addForeignKey('fk_rel_tokens_to_parent_token', $this->table_name, 'parent_id', 'sys_users_tokens', 'id', 'CASCADE', 'CASCADE');
+		$this->addForeignKey('fk_rel_tokens_to_child_token', $this->table_name, 'child_id', 'sys_users_tokens', 'id', 'CASCADE', 'CASCADE');
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeDown() {
+		parent::safeDown();
+
+		$this->dropForeignKey('fk_rel_tokens_to_child_token', $this->table_name);
+		$this->dropForeignKey('fk_rel_tokens_to_parent_token', $this->table_name);
+	}
+
+}
---
+++ models/sys/users/active_record/relations/RelUsersTokensToTokens.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\models\sys\users\active_record\relations;
+
+use app\models\sys\users\active_record\UsersTokens;
+use pozitronik\relations\traits\RelationsTrait;
+use yii\db\ActiveQuery;
+use yii\db\ActiveRecord;
+
+/**
+ * Class RelUsersTokensToTokens
+ * @package app\models\sys\users\active_record\relations
+ *
+ * @property-read UsersTokens|null $relatedParentToken
+ * @property-read UsersTokens|null $relatedChildToken
+ */
+class RelUsersTokensToTokens extends ActiveRecord
+{
+	use RelationsTrait;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName(): string
+	{
+		return 'sys_relation_users_tokens_to_tokens';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules(): array
+	{
+		return [
+			[['parent_id', 'child_id'], 'required'],
+			[['parent_id', 'child_id'], 'integer'],
+			[['parent_id', 'child_id'], 'unique', 'targetAttribute' => ['parent_id', 'child_id']]
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels(): array
+	{
+		return [
+			'id'        => 'ID',
+			'parent_id' => 'Parent ID',
+			'child_id'  => 'Child ID',
+		];
+	}
+
+	/**
+	 * @return ActiveQuery
+	 */
+	public function getRelatedParentToken(): ActiveQuery
+	{
+		return $this->hasOne(UsersTokens::class, ['id' => 'parent_id']);
+	}
+
+	/**
+	 * @return ActiveQuery
+	 */
+	public function getRelatedChildToken(): ActiveQuery
+	{
+		return $this->hasOne(UsersTokens::class, ['id' => 'child_id']);
+	}
+}
