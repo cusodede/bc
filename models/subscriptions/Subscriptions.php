b03154c3fb6e9c3ee079d87b097cfccb99ebfd54
Ermachenok A.A <php-masters@yandex.ru>
2021-07-06 16:33:33 +0300

    Добавил параметры для продуктов.

--- models/subscriptions/Subscriptions.php
+++ models/subscriptions/Subscriptions.php
@@ -5,11 +5,14 @@ namespace app\models\subscriptions;
 
 use app\models\subscriptions\active_record\Subscriptions as ActiveRecordSubscriptions;
 use yii\helpers\ArrayHelper;
+use Exception;
 
 /**
  * Логика подписок, не относящиеся к ActiveRecord
  * Class Subscriptions
  * @package app\models\subscriptions
+ *
+ * @property-read string $unitName
  */
 class Subscriptions extends ActiveRecordSubscriptions
 {
@@ -24,5 +27,14 @@ class Subscriptions extends ActiveRecordSubscriptions
 			// Валидация только нужных атрибутов, при создании/обновлении ajax
 			self::SCENARIO_CREATE_AJAX => ['trial_count', 'units'],
 		]);
+	}
+
+	/**
+	 * @return string
+	 * @throws Exception
+	 */
+	public function getUnitName(): string
+	{
+		return EnumSubscriptionTrialUnits::getScalar($this->units);
 	}
 }
