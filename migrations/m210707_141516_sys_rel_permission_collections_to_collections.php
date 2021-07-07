a509f8d029733c34262512e084343c3a4790c7b4
Pozitronik <antikillerxxl@gmail.com>
2021-07-07 18:00:51 +0300

    table name fixed

--- migrations/m210707_141516_sys_rel_permission_collections_to_collections.php
+++ migrations/m210707_141516_sys_rel_permission_collections_to_collections.php
@@ -6,7 +6,7 @@ use pozitronik\relations\models\RelationMigration;
  * Class m210707_141516_sys_rel_permission_collections_to_collections
  */
 class m210707_141516_sys_rel_permission_collections_to_collections extends RelationMigration {
-	public string $table_name = 'permissions_collections_to_permissions_collections';
+	public string $table_name = 'sys_relation_permissions_collections_to_permissions_collections';
 	public string $first_key = 'master_id';
 	public string $second_key = 'slave_id';
 
