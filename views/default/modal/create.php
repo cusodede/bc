8a3ecc01492c871cb821e7fcb8db4b9ea0d13339
Alexei Lapin <lapin.alx@gmail.com>
2021-07-22 13:03:34 +0300

    Скорректировал модалки во вьюхах

--- views/default/modal/create.php
+++ views/default/modal/create.php
@@ -4,9 +4,9 @@ declare(strict_types = 1);
 /**
  * @var View $this
  * @var Model $model
+ * @var string|null $title
  */
 
-use pozitronik\widgets\BadgeWidget;
 use yii\base\Model;
 use yii\bootstrap4\Modal;
 use yii\web\JsExpression;
@@ -16,29 +16,26 @@ use yii\bootstrap4\ActiveForm;
 $modelName = $model->formName();
 ?>
 <?php Modal::begin([
-	'id' => "{$modelName}-modal-create-new",
-	'size' => Modal::SIZE_LARGE,
-	'title' => BadgeWidget::widget([
-		'items' => $model,
-		'subItem' => 'id'
-	]),
-	'footer' => $this->render('../subviews/editPanelFooter', [
+	'id'      => "{$modelName}-modal-create-new",
+	'size'    => Modal::SIZE_LARGE,
+	'title'   => $title ?? '',
+	'footer'  => $this->render(Yii::$app->controller->viewPath . '/subviews/editPanelFooter', [
 		'model' => $model,
-		'form' => "{$modelName}-modal-create"
-	]),//post button outside the form
+		'form'  => "{$modelName}-modal-create"
+	]),
 	'options' => [
 		'class' => 'modal-dialog-large',
 	]
 ]); ?>
 <?php $form = ActiveForm::begin(
 	[
-		'id' => "{$modelName}-modal-create",
+		'id'                   => "{$modelName}-modal-create",
 		'enableAjaxValidation' => true,
-		'options' => [
+		'options'              => [
 			"onsubmit" => new JsExpression("formSubmitAjax(event)")
 		]
 	])
 ?>
-<?= $this->render('../subviews/editPanelBody', compact('model', 'form')) ?>
+<?= $this->render(Yii::$app->controller->viewPath . '/subviews/editPanelBody', compact('model', 'form')) ?>
 <?php ActiveForm::end(); ?>
 <?php Modal::end(); ?>
--- views/subscriptions/modal/create.php
+++ views/subscriptions/modal/create.php
@@ -6,29 +6,9 @@ declare(strict_types = 1);
  * @var Subscriptions $model
  */
 
-use yii\bootstrap4\Modal;
 use yii\web\View;
-use kartik\form\ActiveForm;
 use app\models\subscriptions\Subscriptions;
 
-$modelName = $model->formName();
 ?>
-<?php Modal::begin([
-	'id' => "{$modelName}-modal-create-new",
-	'size' => Modal::SIZE_LARGE,
-	'title' => 'Новая подписка',
-	'footer' => $this->render('../subviews/editPanelFooter', [
-		'model' => $model,
-		'form' => "{$modelName}-modal-create"
-	]),
-	'options' => [
-		'class' => 'modal-dialog-large',
-	]
-]); ?>
-<?php $form = ActiveForm::begin([
-	'id' => "{$modelName}-modal-create",
-	'enableAjaxValidation' => true,
-]); ?>
-<?= $this->render('../subviews/editPanelBody', compact('model', 'form')) ?>
-<?php ActiveForm::end(); ?>
-<?php Modal::end(); ?>
+
+<?= $this->render('@app/views/default/modal/create', ['model' => $model, 'title' => 'Создание подписки']) ?>
