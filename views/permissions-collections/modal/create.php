b3b2ca9fdbff3f66d5e5fbc28b340cfe6eba21df
Alexei Lapin <lapin.alx@gmail.com>
2021-07-22 14:26:04 +0300

    fix

--- views/permissions-collections/modal/create.php
+++ views/permissions-collections/modal/create.php
@@ -7,36 +7,8 @@ declare(strict_types = 1);
  */
 
 use app\models\sys\permissions\PermissionsCollections;
-use pozitronik\widgets\BadgeWidget;
-use yii\bootstrap4\Modal;
-use yii\web\JsExpression;
 use yii\web\View;
-use yii\bootstrap4\ActiveForm;
 
 ?>
-<?php Modal::begin([
-	'id' => "{$model->formName()}-modal-create-new",
-	'size' => Modal::SIZE_LARGE,
-	'title' => BadgeWidget::widget([
-		'items' => $model,
-		'subItem' => 'name'
-	]),
-	'footer' => $this->render('../subviews/editPanelFooter', [
-		'model' => $model,
-		'form' => "{$model->formName()}-modal-create"
-	]),//post button outside the form
-	'options' => [
-		'class' => 'modal-dialog-large',
-	]
-]); ?>
-<?php $form = ActiveForm::begin(
-	[
-		'id' => "{$model->formName()}-modal-create",
-		'enableAjaxValidation' => true,
-		'options' => [
-			"onsubmit" => new JsExpression("formSubmitAjax(event)")
-		]
-	]) ?>
-<?= $this->render('../subviews/editPanelBody', compact('model', 'form')) ?>
-<?php ActiveForm::end(); ?>
-<?php Modal::end(); ?>
+
+<?= $this->render('@app/views/default/modal/create', ['model' => $model, 'title' => 'Создание группы пермиссий']) ?>
