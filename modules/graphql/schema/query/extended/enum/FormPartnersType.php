abb3f536e3488f0de663a4e70387c8c839bd0df8
Ermachenok A.A <php-masters@yandex.ru>
2021-07-29 11:08:36 +0300

    Revert.

--- modules/graphql/schema/query/extended/enum/FormPartnersType.php
+++ modules/graphql/schema/query/extended/enum/FormPartnersType.php
@@ -11,18 +11,26 @@ use GraphQL\Type\Definition\EnumType;
  */
 class FormPartnersType extends EnumType
 {
+	public const NAME			= 'name';
+	public const INN			= 'inn';
+	public const PHONE 			= 'phone';
+	public const EMAIL			= 'email';
+	public const COMMENT		= 'comment';
+	public const CATEGORY_ID	= 'category_id';
+	public const LOGO			= 'logo';
+
 	public function __construct()
 	{
 		parent::__construct([
 			'name' => 'FormPartners',
 			'values' => [
-				'name',
-				'inn',
-				'phone',
-				'email',
-				'comment',
-				'category_id',
-				'logo',
+				self::NAME,
+				self::INN,
+				self::PHONE,
+				self::EMAIL,
+				self::COMMENT,
+				self::CATEGORY_ID,
+				self::LOGO,
 			],
 		]);
 	}
