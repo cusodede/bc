9a88d4fda83a57c56b69a3efdf8e9c9742b353f3
Pozitronik <antikillerxxl@gmail.com>
2021-08-03 12:04:31 +0300

    cs

--- web/index.php
+++ web/index.php
@@ -3,8 +3,8 @@ declare(strict_types = 1);
 
 require __DIR__.'/../config/env.php';
 require __DIR__.'/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+require __DIR__.'/../vendor/yiisoft/yii2/Yii.php';
 
-$config = require __DIR__ . '/../config/web.php';
+$config = require __DIR__.'/../config/web.php';
 
 (new yii\web\Application($config))->run();
