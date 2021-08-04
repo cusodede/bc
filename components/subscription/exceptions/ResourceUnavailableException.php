1fa9f2736079debfe481431900dd7526fb4bd1d7
Alexei Lapin <lapin.alx@gmail.com>
2021-08-04 11:40:19 +0300

    Добавлены эксеншены для обработчиков управления подпиской

---
+++ components/subscription/exceptions/ResourceUnavailableException.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\components\subscription\exceptions;
+
+use Exception;
+
+/**
+ * TBD
+ */
+class ResourceUnavailableException extends Exception
+{
+
+}
