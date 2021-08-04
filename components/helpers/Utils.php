324689a73ef4e4d4bb1b3ebaac4db9351008898d
Pozitronik <antikillerxxl@gmail.com>
2021-08-04 22:54:19 +0300

    Название по феншую Наследование от моего щеголеватого хелпера

--- components/helpers/Utils.php
+++ components/helpers/Utils.php
@@ -3,18 +3,19 @@ declare(strict_types = 1);
 
 namespace app\components\helpers;
 
+use pozitronik\helpers\Utils as VendorUtils;
 use Throwable;
 
 /**
  * Хелпер для всяких замысловатых штук.
  */
-class Utils
+class Utils extends VendorUtils
 {
 	/**
 	 * @param string $url
 	 * @return bool
 	 */
-	public static function doUrlHealthcheck(string $url): bool
+	public static function doUrlHealthCheck(string $url): bool
 	{
 		try {
 			$headers = get_headers($url);
