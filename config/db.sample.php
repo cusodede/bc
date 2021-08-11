35af9170547a64c36acfdefb7bd6a5fbc3082fa5
MoiseenkoVA <VAMoiseenko@beeline.ru>
2021-08-11 13:22:18 +0300

    Изменения конфигурации, для автоматического подключения к бд с нужными настройками из под докер контейнера.

--- config/db.sample.php
+++ config/db.sample.php
@@ -3,9 +3,9 @@ declare(strict_types = 1);
 
 return [
 	'class' => 'yii\db\Connection',
-	'dsn' => 'mysql:host=localhost;dbname=bc',
-	'username' => 'root',
-	'password' => 'root',
+	'dsn' => 'mysql:host=' . getenv('MYSQL_HOST') . ';dbname='. getenv('MYSQL_DATABASE'),
+	'username' => getenv('MYSQL_ROOT_USERNAME'),
+	'password' => getenv('MYSQL_ROOT_PASSWORD'),
 	'charset' => 'utf8',
 
 	// Schema cache options (for production environment)
