a164e7cd13c7dfb6d729467cb12887cc7fcd0e19
Pozitronik <antikillerxxl@gmail.com>
2021-08-12 12:15:43 +0300

    Всё-таки пусть будет тут типизация

--- components/db/ActiveRecordTrait.php
+++ components/db/ActiveRecordTrait.php
@@ -37,7 +37,7 @@ trait ActiveRecordTrait {
 	 * @param int|string|ActiveRecordInterface $model
 	 * @return ActiveRecordInterface|null
 	 */
-	public static function ensureModel($className, $model):?ActiveRecordInterface {
+	public static function ensureModel(string|ActiveRecordInterface $className, int|string|ActiveRecordInterface $model):?ActiveRecordInterface {
 		if (is_string($model) && is_numeric($model)) {
 			$model = (int)$model;
 		}
