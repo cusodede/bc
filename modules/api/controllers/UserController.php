4ce1184646a25984fd59b210c2dde52a1d4fa1ef
Alexei Lapin <lapin.alx@gmail.com>
2021-08-18 09:46:58 +0300

    cosmetics

--- modules/api/controllers/UserController.php
+++ modules/api/controllers/UserController.php
@@ -77,6 +77,7 @@ class UserController extends Controller
 	public function actionCheckRestorePasswordCode(string $code): void
 	{
 		$user = Users::findByRestoreCode($code);
+
 		if ((null === $user) || $user->isExpiredRestoreCode()) {
 			throw new HttpException(500);
 		}
