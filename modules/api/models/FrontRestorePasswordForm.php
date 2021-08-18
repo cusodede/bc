9ede64a37b4f16e7d9ecc667465bffd6319f8b53
Pozitronik <antikillerxxl@gmail.com>
2021-08-18 13:52:07 +0300

    Защита от дурачка

--- modules/api/models/FrontRestorePasswordForm.php
+++ modules/api/models/FrontRestorePasswordForm.php
@@ -4,7 +4,10 @@ declare(strict_types = 1);
 namespace app\modules\api\models;
 
 use app\models\site\RestorePasswordForm;
+use pozitronik\helpers\ArrayHelper;
+use Throwable;
 use Yii;
+use yii\base\InvalidConfigException;
 
 /**
  * Class FrontRestorePasswordForm
@@ -13,9 +16,10 @@ class FrontRestorePasswordForm extends RestorePasswordForm
 {
 	/**
 	 * {@inheritDoc}
+	 * @throws Throwable
 	 */
 	public static function getRestoreUrl(string $restoreCode): string
 	{
-		return Yii::$app->params['frontUrl'] . '/set-password?' . http_build_query(['code' => $restoreCode]);
+		return ArrayHelper::getValue(Yii::$app->params, 'frontUrl', new InvalidConfigException('params.frontUrl parameter not set')) . '/set-password?' . http_build_query(['code' => $restoreCode]);
 	}
 }
