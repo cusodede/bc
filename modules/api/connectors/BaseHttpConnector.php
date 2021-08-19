c558c31d51e9b505064e48fe377d6e4d5fe4769a
Alexei Lapin <lapin.alx@gmail.com>
2021-08-19 18:02:28 +0300

    add few abstract layers to BaseHttpConnector.php

--- modules/api/connectors/BaseHttpConnector.php
+++ modules/api/connectors/BaseHttpConnector.php
@@ -3,19 +3,28 @@ declare(strict_types = 1);
 
 namespace app\modules\api\connectors;
 
+use Exception;
 use yii\base\Component;
+use yii\helpers\ArrayHelper;
 use yii\httpclient\Client;
 use yii\httpclient\CurlTransport;
+use yii\httpclient\Exception as HttpClientException;
+use yii\httpclient\Response;
 
 /**
  * Class BaseConnector
  * @package app\modules\api\connectors
  *
+ * @property-read Client $client
+ * @property-read null|Response $response
+ * @property-read bool $responseIsOk
  * @property-read string $baseUrl
  */
 abstract class BaseHttpConnector extends Component
 {
 	private Client $_httpClient;
+
+	private ?Response $_response = null;
 
 	/**
 	 * BaseHttpConnector constructor.
@@ -29,11 +38,71 @@ abstract class BaseHttpConnector extends Component
 	}
 
 	/**
+	 * @param array|string $url
+	 * @param array|string|null $data
+	 * @param array $headers
+	 * @param array $options
+	 * @throws HttpClientException
+	 */
+	public function get(
+		array|string $url,
+		array|string $data = null,
+		array $headers = [],
+		array $options = []
+	): void
+	{
+		$this->_response = $this->_httpClient->get($url, $data, $headers, $options)->send();
+	}
+
+	/**
+	 * @param array|string $url
+	 * @param array|string|null $data
+	 * @param array $headers
+	 * @param array $options
+	 * @throws HttpClientException
+	 */
+	public function post(
+		array|string $url,
+		array|string $data = null,
+		array $headers = [],
+		array $options = []
+	): void
+	{
+		$this->_response = $this->_httpClient->post($url, $data, $headers, $options)->send();
+	}
+
+	/**
 	 * @return Client
 	 */
 	protected function getClient(): Client
 	{
 		return $this->_httpClient;
+	}
+
+	/**
+	 * @return Response|null
+	 */
+	protected function getResponse(): ?Response
+	{
+		return $this->_response;
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function getResponseIsOk(): bool
+	{
+		return $this->_response->isOk;
+	}
+
+	/**
+	 * @param string $key
+	 * @return mixed
+	 * @throws Exception
+	 */
+	protected function extractResponseData(string $key): mixed
+	{
+		return ArrayHelper::getValue($this->response->data, $key);
 	}
 
 	/**
