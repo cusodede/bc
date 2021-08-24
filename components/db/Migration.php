cd9f321516b03088aa62a1cb6d9c54c8ad5c6601
Pozitronik <antikillerxxl@gmail.com>
2021-08-24 16:22:57 +0300

    Исправлено пересоздание функции Фикс SQL

--- components/db/Migration.php
+++ components/db/Migration.php
@@ -18,7 +18,7 @@ class Migration extends VendorMigration
 	public function createOnUpdateTrigger(string $tableName): void
 	{
 		if ('pgsql' === $this->db->driverName) {
-			$this->execute("CREATE update_updated_at TRIGGER BEFORE UPDATE ON {$tableName} FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column()");
+			$this->execute("CREATE TRIGGER update_updated_at  BEFORE UPDATE ON {$tableName} FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column()");
 //			if (false === (bool)$this->db->createCommand("select trigger_name from information_schema.triggers where event_object_table='{$tableName}' and trigger_name = 'update_updated_at'")->queryScalar()) {
 //				throw new Exception("Не удалось создать триггер для таблицы {$tableName}");
 //			}
@@ -28,7 +28,7 @@ class Migration extends VendorMigration
 	public function update_updated_at_column(): void
 	{
 		if ('pgsql' === $this->db->driverName) {
-			$this->execute("CREATE FUNCTION update_updated_at_column() RETURNS TRIGGER AS $$ 
+			$this->execute("CREATE OR REPLACE FUNCTION update_updated_at_column() RETURNS TRIGGER AS $$ 
 BEGIN
     NEW.updated_at = now();
     RETURN NEW;
