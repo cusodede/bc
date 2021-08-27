844baf33bd557f5c7bf8164a1355286d3e66c1ec
MoiseenkoVA <VAMoiseenko@beeline.ru>
2021-08-27 16:24:48 +0300

    PRPL-102: Доработка сущности Партнера
    - (changed) В формы добавлены поля выбора.

--- views/contracts/subviews/editPanelBody.php
+++ views/contracts/subviews/editPanelBody.php
@@ -7,8 +7,11 @@ declare(strict_types = 1);
  * @var ActiveForm $form
  */
 
+use app\models\products\Products;
 use kartik\date\DatePicker;
 use kartik\form\ActiveForm;
+use kartik\select2\Select2;
+use pozitronik\helpers\ArrayHelper;
 use yii\base\Model;
 use yii\web\View;
 
@@ -16,6 +19,15 @@ use yii\web\View;
 
 <?= $form->field($model, 'contract_number')->textInput() ?>
 <?= $form->field($model, 'contract_number_nfs')->textInput() ?>
+<?= $form->field($model, 'relatedProducts')->widget(Select2::class, [
+	'data' => ArrayHelper::map(Products::find()->active()->all(), 'id', 'name'),
+	'pluginOptions' => [
+		'placeholder' => 'Выберите продукты',
+		'multiple' => true,
+		'allowClear' => true,
+		'tags' => true
+	]
+]) ?>
 <?= $form->field($model, 'signing_date')->widget(DatePicker::class, [
 	'model' => $model,
 	'options' => ['placeholder' => 'Enter event time ...'],
