8d967408a9593368e13b6032dea333d78d3732f9
MoiseenkoVA <VAMoiseenko@beeline.ru>
2021-08-28 22:06:44 +0300

    PRPL-102: Доработка сущности Партнера
    - (refactoring) Поменял наследование. Класс миграции отнаследован от специального класса для связанных таблиц.

--- migrations/m210826_090541_create_table_rel_contracts_to_products.php
+++ migrations/m210826_090541_create_table_rel_contracts_to_products.php
@@ -1,38 +1,23 @@
 <?php
 declare(strict_types = 1);
 
-use yii\db\Migration;
+use pozitronik\relations\models\RelationMigration;
 
 /**
 * Class m210826_090541_create_table_rel_contracts_to_products
 */
-class m210826_090541_create_table_rel_contracts_to_products extends Migration {
-	private const TABLE_NAME = 'relation_contracts_to_products';
-
+class m210826_090541_create_table_rel_contracts_to_products extends RelationMigration {
+	public string $table_name = 'relation_contracts_to_products';
+	public string $first_key = 'contract_id';
+	public string $second_key = 'product_id';
 
 	/**
 	 * {@inheritdoc}
 	 */
 	public function safeUp()
 	{
-		$this->createTable(self::TABLE_NAME, [
-			'id' => $this->primaryKey(),
-			'contract_id' => $this->integer()->notNull()->comment('id договора'),
-			'product_id' => $this->integer()->notNull()->comment('id продукта'),
-		]);
-
+		parent::safeUp();
 		$this->addForeignKey('fk_rel_tab_to_contracts', 'relation_contracts_to_products', 'contract_id', 'contracts', 'id');
 		$this->addForeignKey('fk_rel_tab_to_products', 'relation_contracts_to_products', 'product_id', 'products', 'id');
 	}
-
-	/**
-	 * {@inheritdoc}
-	 */
-	public function safeDown()
-	{
-		$this->dropForeignKey('fk_rel_tab_to_contracts', 'contracts');
-		$this->dropForeignKey('fk_rel_tab_to_products', 'products');
-		$this->dropTable(self::TABLE_NAME);
-	}
-
 }
