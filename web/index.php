4f1bd99b9d2575319bc90e078e54f1c68078d50d
Ponkratenko EV <eponkratenko@beeline.ru>
2021-09-02 11:19:38 +0300

    конфиги тестов

--- web/index.php
+++ web/index.php
@@ -4,12 +4,13 @@ declare(strict_types = 1);
 use Dotenv\Dotenv;
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 #load env
 Dotenv::createImmutable(__DIR__ . '/../')->safeLoad();
 
 require __DIR__ . '/../config/env.php';
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
 $config = require __DIR__ . '/../config/web.php';
 
 (new yii\web\Application($config))->run();
