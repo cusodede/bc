e68934bb9a3d12539427c1618f57bd500b989268
Alexei Lapin <alslapin@beeline.ru>
2021-09-02 10:57:44 +0300

    refactor migrations for pgsql

--- migrations/m210825_140829_create_refsharing_rates_table.php
+++ migrations/m210825_140829_create_refsharing_rates_table.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types = 1);
 
-use yii\db\Migration;
+use app\components\db\Migration;
 
 /**
  * Handles the creation of table refsharing_rates.
@@ -22,8 +22,10 @@ class m210825_140829_create_refsharing_rates_table extends Migration
 			'value' => $this->integer()->notNull()->comment('Значение процентной ставки'),
 			'deleted' => $this->boolean()->notNull()->defaultValue(false)->comment('Флаг активности'),
 			'created_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP')->notNull()->comment('Дата создания договора'),
-			'updated_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')->notNull()->comment('Дата обновления договора'),
+			'updated_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP')->notNull()->comment('Дата обновления договора'),
 		]);
+
+		$this->createOnUpdateTrigger(self::TABLE_NAME);
 
 		$this->createIndex('idx-refsharing-deleted', 'refsharing_rates', 'deleted');
 	}
