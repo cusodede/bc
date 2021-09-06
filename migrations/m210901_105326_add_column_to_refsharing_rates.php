ecdeeb304220020d62a4514c9902686e2f33c8ab
Ermachenok A.A <php-masters@yandex.ru>
2021-09-06 17:59:52 +0300

    fix миграции.

--- migrations/m210901_105326_add_column_to_refsharing_rates.php
+++ migrations/m210901_105326_add_column_to_refsharing_rates.php
@@ -12,15 +12,15 @@ class m210901_105326_add_column_to_refsharing_rates extends Migration {
 	 * {@inheritdoc}
 	 */
 	public function safeUp() {
-		$this->addColumn(RevShareRates::tableName(), 'product_id', $this->integer()->notNull()->after('id')->comment('id продукта'));
-		$this->addForeignKey('fk_ref_share_product_id', RevShareRates::tableName(), 'product_id', 'products', 'id', 'CASCADE');
+		$this->addColumn('refsharing_rates', 'product_id', $this->integer()->notNull()->after('id')->comment('id продукта'));
+		$this->addForeignKey('fk_ref_share_product_id', 'refsharing_rates', 'product_id', 'products', 'id', 'CASCADE');
 	}
 
 	/**
 	 * {@inheritdoc}
 	 */
 	public function safeDown() {
-		$this->dropForeignKey('fk_ref_share_product_id', RevShareRates::tableName());
-		$this->dropColumn(RevShareRates::tableName(), 'product_id');
+		$this->dropForeignKey('fk_ref_share_product_id', 'refsharing_rates');
+		$this->dropColumn('refsharing_rates', 'product_id');
 	}
 }
