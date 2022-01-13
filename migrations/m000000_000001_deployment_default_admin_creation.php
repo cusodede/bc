091944787bbef08c186088472c92d51fb07c6f33
Pozitronik <antikillerxxl@gmail.com>
2022-01-13 13:32:48 +0300

    Проверка не требуется

--- migrations/m000000_000001_deployment_default_admin_creation.php
+++ migrations/m000000_000001_deployment_default_admin_creation.php
@@ -5,25 +5,22 @@ use yii\db\Expression;
 use app\components\db\Migration;
 
 /**
- * Class m210805_084918_deployment_default_admin_creation
+ * Class m000000_000001_deployment_default_admin_creation
  */
 class m000000_000001_deployment_default_admin_creation extends Migration {
 	/**
 	 * {@inheritdoc}
 	 */
 	public function safeUp() {
-		if (0 === (int)Users::find()->count()) {
-			$this->insert(Users::tableName(), [
-//				'id' => 1,
-				'username' => 'admin',
-				'login' => 'admin',
-				'password' => 'admin',
-				'is_pwd_outdated' => 1, /*require to update admin password*/
-				'email' => 'admin@localhost.ru',
-				'comment' => 'Системный администратор',
-				'create_date' => new Expression('NOW()')
-			]);
-		}
+		$this->insert(Users::tableName(), [
+			'username' => 'admin',
+			'login' => 'admin',
+			'password' => 'admin',
+			'is_pwd_outdated' => 1, /*require to update admin password*/
+			'email' => 'admin@localhost.ru',
+			'comment' => 'Системный администратор',
+			'create_date' => new Expression('NOW()')
+		]);
 	}
 
 	/**
