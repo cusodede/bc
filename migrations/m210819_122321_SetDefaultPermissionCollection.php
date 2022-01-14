c88c9197ab71f2af7ed64ddfad7cb16323914071
Pozitronik <antikillerxxl@gmail.com>
2022-01-14 14:07:04 +0300

    Миграция на дефолтную группу доступа

---
+++ migrations/m210819_122321_SetDefaultPermissionCollection.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types = 1);
+use app\components\db\Migration;
+
+/**
+ * Class m210819_122321_SetDefaultPermissionCollection
+ */
+class m210819_122321_SetDefaultPermissionCollection extends Migration {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeUp() {
+		$this->addColumn('sys_permissions_collections', 'default', $this->boolean()->defaultValue(false)->notNull()->comment('Включение группы по умолчанию'));
+
+		$this->createIndex('sys_permissions_collections_default', 'sys_permissions_collections', 'default');
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeDown() {
+		$this->dropColumn('default', 'sys_permissions_collections');
+	}
+
+}
